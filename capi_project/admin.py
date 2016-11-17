9d16bb8dfc7b991e476fe87f0835fa58c9ac0cb6
Anastasia <anastasia.aizman@gmail.com>
2016-11-17 12:57:59 -0500

    add - format admin site, add api key into user model

--- capi_project/admin.py
+++ capi_project/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from django.db.models import Case
 from .models import *
-admin.site.register(Case)
+
+class CaseUserAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_joined','api_key')
+    list_display = ('email', 'last_name', 'first_name', 'api_key')
+    def api_key(self, instance):
+        return instance.get_api_key()
+    api_key.short_description = "API Key"
+    fields = ('email', 'first_name', 'last_name', 'case_allowance', 'date_joined', 'api_key', 'is_validated', 'is_researcher', 'is_staff')
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ('name_abbreviation', 'citation')
+
+
+admin.site.register(Case, CaseAdmin)
+admin.site.register(CaseUser, CaseUserAdmin)
