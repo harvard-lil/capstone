eeda8e3ae06e6da187debd02d13e754a9fad8802
Anastasia <anastasia.aizman@gmail.com>
2016-11-30 10:30:27 -0500

    fix - write metadata success message

--- capi_project/management/commands/write_metadata.py
+++ capi_project/management/commands/write_metadata.py
@@ -24,4 +24,4 @@ class Command(BaseCommand):
                             error_raised = True
                             pass
                 if not error_raised:
-                    self.stdout.write(self.style.SUCCESS('Success', os.path.join(root, filename)))
+                    self.stdout.write(self.style.SUCCESS("Success %s" % os.path.join(root, filename)))
