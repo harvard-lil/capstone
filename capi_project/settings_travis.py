b2cf09df353cc3ef666756702d6fc33d10e80b08
Anastasia <anastasia.aizman@gmail.com>
2016-12-06 17:07:07 -0500

    travis

--- capi_project/settings_travis.py
+++ capi_project/settings_travis.py
@@ -77,9 +77,7 @@ WSGI_APPLICATION = 'capi_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cap',
-        'USER': 'capuser',
-        'PASSWORD': 'test',
+        'NAME': 'cap_db',
         'HOST': '',
         'PORT': '',
     }
