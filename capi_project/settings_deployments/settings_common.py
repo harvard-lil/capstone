58700be80bbe1dbc907cdf26d7ef8006d65e2ed0
Anastasia <anastasia.aizman@gmail.com>
2016-12-08 16:04:38 -0500

    fix -  remove whitenoise middleware

--- capi_project/settings_deployments/settings_common.py
+++ capi_project/settings_deployments/settings_common.py
@@ -94,7 +94,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 TEMPLATES = [
