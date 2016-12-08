772d6405cdd53e83ccc98f84b7dbc9aa2a365e39
Anastasia <anastasia.aizman@gmail.com>
2016-12-08 16:54:53 -0500

    update - whitenoise wsgi app

--- capi_project/wsgi.py
+++ capi_project/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for capi_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capi_project.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
