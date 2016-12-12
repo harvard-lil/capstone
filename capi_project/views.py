baae7411eab2f45290921295319465739fee76aa
Anastasia <anastasia.aizman@gmail.com>
2016-12-12 15:02:29 -0500

    update - add resending nonce ability / signup flow

--- capi_project/views.py
+++ capi_project/views.py
@@ -62,7 +62,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 return Response({'errors':serializer.errors}, template_name='sign-up-success.html', status=status.HTTP_400_BAD_REQUEST)
         else:
             print serializer.errors
-            return Response({'serializer':serializer, 'errors':serializer.errors}, template_name='sign-up-success.html', status=status.HTTP_400_BAD_REQUEST)
+            return Response({'serializer':serializer, 'errors':serializer.errors}, template_name='sign-up.html', status=status.HTTP_400_BAD_REQUEST)
 
     @list_route(methods=['get'], permission_classes=[AllowAny])
     def login(self, request):
@@ -76,19 +76,41 @@ class UserViewSet(viewsets.ModelViewSet):
             try:
                 user = serializer.verify_with_password(email=request.data.get('email'), password=request.data.get('password'))
 
-                # update case allowance before sending back
-                user.update_case_allowance()
-                user = CaseUser.objects.get(email=user.email)
+                api_key = user.get_api_key()
+                if api_key:
+                    # update case allowance before sending back
+                    user.update_case_allowance()
+                    user = CaseUser.objects.get(email=user.email)
+                    print user.is_authenticated()
+                    return Response({'email':user.email, 'api_key':user.get_api_key(), 'case_allowance':user.case_allowance}, template_name='user-account.html',)
+                else:
 
-                return Response({'email':user.email, 'api_key':user.get_api_key, 'case_allowance':user.case_allowance}, template_name='user-account.html',)
+                    return Response({'user_id':user.id, 'user_email':user.email, 'info_email':settings.EMAIL_ADDRESS}, template_name='resend-nonce.html',)
             except Exception as e:
                 content = {'errors':{'messages':'Invalid password or email address'}, 'serializer':serializer}
                 return Response(content, template_name='log-in.html', status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({'errors':serializer.errors}, template_name='error.html', status=status.HTTP_400_BAD_REQUEST)
+            return Response({'errors':serializer.errors, 'serializer':serializer}, template_name='log-in.html', status=status.HTTP_400_BAD_REQUEST)
+
+    @list_route(methods=['post'], permission_classes=[IsCaseUser])
+    def resend_verification(self, request, *args, **kwargs):
+
+        user = CaseUser.objects.get(email=request.data.get('user_email'))
+        if str(user.id) == request.data.get('user_id'):
+
+            email(reason='new_signup', user=user)
+            content = {
+            'status':'Success!',
+            'message':'Thank you. Please check your email %s for a verification link.' % user.email}
+            return Response(content, template_name='sign-up-success.html')
+
+        else:
+            raise Exception("Attempted sending email with unmatched credentials %s %s" % (user.id, request.data.get('user_email')))
+            pass
+            return Response({'errors':'Uh oh. Something went wrong.',}, template_name='errors.html', status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(http_method_names=['GET'])
-@renderer_classes((renderers.BrowsableAPIRenderer,renderers.JSONRenderer,))
+@renderer_classes((renderers.TemplateHTMLRenderer,renderers.JSONRenderer,))
 def verify_user(request, user_id, activation_nonce):
     """
     Verify email and assign api token
@@ -98,34 +120,8 @@ def verify_user(request, user_id, activation_nonce):
     if user.is_authenticated():
         email(reason='new_registration', user=user)
         data = {'status':'Success!','message':'Thank you for verifying your email address. We will be in touch with you shortly.'}
-        if request.accepted_renderer.format == 'html' :
-            return Response(data, template_name='verified.html')
-        elif request.accepted_renderer.format == 'api' :
-            return Response(data)
-        else:
+        if request.accepted_renderer.format == 'json' :
             return JSONResponse(data)
+        return Response(data, template_name='verified.html')
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(http_method_names=['GET','POST'])
-@parser_classes((FormParser, MultiPartParser, JSONParser,))
-@renderer_classes((renderers.BrowsableAPIRenderer, renderers.TemplateHTMLRenderer, renderers.JSONRenderer, ))
-def get_token(request):
-    serializer = LoginSerializer()
-    if request.method == 'GET':
-        return Response({'serializer':serializer}, template_name='get-token.html')
-    elif request.method == 'POST':
-        try:
-            email = request.data.get('email')
-            password = request.data.get('password')
-            user = serializer.verify_with_password(email, password)
-            if user.is_authenticated():
-                api_key = user.get_api_key()
-                data = {'email':user.email,'api_key':api_key, 'case_allowance':user.case_allowance}
-                if request.accepted_renderer.format != 'json' :
-                    return Response(data, template_name='user-account.html')
-                else:
-                    return JSONResponse(data)
-        except Exception as e:
-            data = {'status':'Failed.','message':'Please check that your entered credentials are correct.'}
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
