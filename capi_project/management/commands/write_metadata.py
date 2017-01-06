a05be4bc6739d8cd43373348cfd4461e0904d843
Anastasia <anastasia.aizman@gmail.com>
2017-01-06 16:48:43 -0500

    update - writing metadata logic (for now)

--- capi_project/management/commands/write_metadata.py
+++ capi_project/management/commands/write_metadata.py
@@ -13,18 +13,19 @@ class Command(BaseCommand):
         for root, dirs, files in os.walk(settings.METADATA_DIR_PATH):
             for filename in files:
                 try:
-                    with open(os.path.join(root, filename), 'rb') as f:
-                        reader = csv.DictReader(f)
-                        for i,row in enumerate(reader):
-                            try:
-                                models.Case.create_from_row(row)
-
-                            except Exception as e:
-                                self.stdout.write("Error in row %s: %s %s %s" % (os.path.join(root, filename)), e,i,row)
-                                pass
+                    if 'volmets' not in filename and 'metadata_dump' in filename:
+                        self.stdout.write(self.style.SUCCESS("PARSING FILE %s" % filename))
+                        with open(os.path.join(root, filename), 'rb') as f:
+                            reader = csv.DictReader(f)
+                            for i,row in enumerate(reader):
+                                try:
+                                    models.Case.create_from_row(row)
+                                except Exception as e:
+                                    self.stdout.write(self.style.ERROR("Error in row %s: %s %s %s" % (os.path.join(root, filename)), e,i,row))
+                                    pass
                     self.stdout.write(self.style.SUCCESS("Success %s" % os.path.join(root, filename)))
                     # move file to archive directory
                     os.rename(os.path.join(root, filename), os.path.join(settings.METADATA_ARCHIVE_DIR_PATH, filename))
                 except Exception as e:
-                    self.stdout.write("Error in file %s:%s" % (os.path.join(root, filename), e))
+                    self.stdout.write(self.style.ERROR("Error in file %s:%s" % (os.path.join(root, filename), e)))
                     pass
