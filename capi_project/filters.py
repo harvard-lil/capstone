abf96fb825383f96b2170da138bc5d063e47be35
Anastasia <anastasia.aizman@gmail.com>
2017-01-08 00:52:37 -0500

    fix - filters

--- capi_project/filters.py
+++ capi_project/filters.py
@@ -1,22 +1,22 @@
 import django_filters
-from .models import Case
+from .models import Case, Jurisdiction
 from .serializers import CaseSerializer
 from rest_framework import generics
 
 class CaseFilter(django_filters.rest_framework.FilterSet):
-    jurisdiction = django_filters.CharFilter(name="jurisdiction__name", lookup_expr='iexact')
-    court = django_filters.CharFilter(name="court__name", lookup_expr='icontains')
-    name = django_filters.CharFilter(name="name", lookup_expr='icontains')
+    name_contains = django_filters.CharFilter(name="name", lookup_expr='icontains')
+    name_abbreviation_contains = django_filters.CharFilter(name="slug", lookup_expr='icontains')
     citation = django_filters.CharFilter(name="citation", lookup_expr='icontains')
+    court_name_contains = django_filters.CharFilter(name="court__name", lookup_expr='icontains')
     min_year = django_filters.NumberFilter(name="decisiondate", lookup_expr='gte')
     max_year = django_filters.NumberFilter(name="decisiondate", lookup_expr='lte')
-    reporter = django_filters.CharFilter(name="reporter__name", lookup_expr='iexact')
-    slug = django_filters.CharFilter(name="slug", lookup_expr='icontains')
-
+    reporter_name_contains = django_filters.CharFilter(name="reporter__name", lookup_expr='iexact')
     jurisdiction = django_filters.ModelChoiceFilter(
         name='jurisdiction', lookup_expr='isnull',
         queryset=Jurisdiction.objects.all().order_by('name'),
     )
     class Meta:
         model = Case
-        fields = ['jurisdiction', 'court', 'name', 'citation']
+        fields = ['name_contains', 'name_abbreviation_contains',
+        'citation', 'jurisdiction', 'court_name_contains', 
+        'min_year', 'max_year', 'reporter_name_contains']
