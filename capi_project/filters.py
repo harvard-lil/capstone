0e1e44a8b60eb8e2a5f53c9f2f25b1f560e6cbe0
Anastasia <anastasia.aizman@gmail.com>
2017-01-09 16:21:29 -0500

    update - filtering

--- capi_project/filters.py
+++ capi_project/filters.py
@@ -4,19 +4,20 @@ from .serializers import CaseSerializer
 from rest_framework import generics
 
 class CaseFilter(django_filters.rest_framework.FilterSet):
-    name_contains = django_filters.CharFilter(name="name", lookup_expr='icontains')
-    name_abbreviation_contains = django_filters.CharFilter(name="slug", lookup_expr='icontains')
+    name = django_filters.CharFilter(name="name", lookup_expr='icontains')
+    name_abbreviation = django_filters.CharFilter(name="slug", lookup_expr='icontains')
     citation = django_filters.CharFilter(name="citation", lookup_expr='icontains')
-    court_name_contains = django_filters.CharFilter(name="court__name", lookup_expr='icontains')
-    min_year = django_filters.NumberFilter(name="decisiondate", lookup_expr='gte')
-    max_year = django_filters.NumberFilter(name="decisiondate", lookup_expr='lte')
-    reporter_name_contains = django_filters.CharFilter(name="reporter__name", lookup_expr='iexact')
+    court_name = django_filters.CharFilter(name="court__name", lookup_expr='icontains')
+    reporter_name = django_filters.CharFilter(name="reporter__name", lookup_expr='icontains')
+    min_year = django_filters.DateTimeFilter(name="decisiondate", lookup_expr='gte')
+    max_year = django_filters.DateTimeFilter(name="decisiondate", lookup_expr='lte')
     jurisdiction = django_filters.ModelChoiceFilter(
         name='jurisdiction', lookup_expr='isnull',
         queryset=Jurisdiction.objects.all().order_by('name'),
     )
     class Meta:
         model = Case
-        fields = ['name_contains', 'name_abbreviation_contains',
-        'citation', 'jurisdiction', 'court_name_contains', 
-        'min_year', 'max_year', 'reporter_name_contains']
+        fields = ['name', 'name_abbreviation',
+        'citation', 'jurisdiction', 'court_name', 'reporter_name',
+        'min_year','max_year',
+        ]
