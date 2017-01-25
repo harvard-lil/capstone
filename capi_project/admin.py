21f26caa4a1b317ee32c2f5e946586f7fddd0695
Anastasia <anastasia.aizman@gmail.com>
2017-01-25 12:57:49 -0500

    fix - admin fields

--- capi_project/admin.py
+++ capi_project/admin.py
@@ -7,7 +7,7 @@ class CaseUserAdmin(admin.ModelAdmin):
     def api_key(self, instance):
         return instance.get_api_key()
     api_key.short_description = "API Key"
-    fields = ('email', 'first_name', 'last_name', 'case_allowance', 'date_joined', 'api_key', 'is_validated', 'is_researcher', 'is_staff')
+    fields = ('email', 'first_name', 'last_name', 'case_allowance', 'date_joined', 'api_key', 'is_researcher', 'is_staff')
 
 class CaseAdmin(admin.ModelAdmin):
     list_display = ('name_abbreviation', 'citation')
