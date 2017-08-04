45cd6f5fb62dce5c6e7ab6cad58d2a9f95577dd1
anastasia <anastasia.aizman@gmail.com>
2017-08-04 11:14:48 -0400

    remove - sassc files

--- capstone/capapi/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capapi.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
