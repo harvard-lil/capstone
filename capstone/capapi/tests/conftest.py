e48374dbeaa79a7e1a08aa6565b5a7aa22420476
anastasia <anastasia.aizman@gmail.com>
2017-08-14 17:06:15 -0400

    remove - print statements and unused fixture load

--- capstone/capapi/tests/conftest.py
+++ capstone/capapi/tests/conftest.py
@@ -4,11 +4,5 @@ from django.core.management import call_command
 
 @pytest.fixture
 def load_parsed_metadata():
-    print("loading parsed metadata")
     call_command('loaddata', 'test_data/parsed_metadata.json')
 
-
-@pytest.fixture
-def load_user_data():
-    print("loading user metadata")
-    call_command('loaddata', 'test_data/user_data.json')
