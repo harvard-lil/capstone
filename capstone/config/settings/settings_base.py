566ff52572030c301c3d610d983cdc899c34523b
anastasia <anastasia.aizman@gmail.com>
2017-08-14 16:36:07 -0400

    fix - show capapi templates

--- capstone/config/settings/settings_base.py
+++ capstone/config/settings/settings_base.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'capapi', 'templates'), os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
