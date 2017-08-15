edfeffeecd073916e43e2da16325330d3865999d
anastasia <anastasia.aizman@gmail.com>
2017-08-15 16:38:16 -0400

    bump - remove not required DIRS setting, add comment

--- capstone/config/settings/settings_base.py
+++ capstone/config/settings/settings_base.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'capapi', 'templates'), os.path.join(BASE_DIR, 'templates'), ],
+        'DIRS': [os.path.join(BASE_DIR, 'capapi', 'templates')], # required by DRF for some reason
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
