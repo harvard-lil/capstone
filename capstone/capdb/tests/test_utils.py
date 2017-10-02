23d4277cd4806899aa581dce349ab9b1258249cd
anastasia <anastasia.aizman@gmail.com>
2017-10-02 17:57:28 -0400

    add - test that checks for collisions in generate_unique_slug utility

--- capstone/capdb/tests/test_utils.py
+++ capstone/capdb/tests/test_utils.py
@@ -6,12 +6,19 @@ from capdb.models import Reporter
 @pytest.mark.django_db
 def test_generate_unique_slug(ingest_metadata):
     unique_name = "Caselaw Access Project"
-    slug1 = utils.generate_unique_slug(Reporter(), unique_name, field="full_name")
 
+    slug1 = utils.generate_unique_slug(Reporter(), unique_name, field="full_name")
     assert slug1 == "caselaw-access-project"
 
     reporter = Reporter.objects.first()
     reporter_name = reporter.full_name
     slug2 = utils.generate_unique_slug(Reporter(), reporter_name, field="full_name")
-
     assert slug2 == utils.slugify(reporter_name)
+
+    # method deals with collisions
+    jurisdiction = Jurisdiction(name='New. State.')
+    jurisdiction.slug = utils.generate_unique_slug(Jurisdiction(), jurisdiction.name)
+    jurisdiction.save()
+
+    new_unique_slug = utils.generate_unique_slug(Jurisdiction(), jurisdiction.name)
+    assert new_unique_slug != jurisdiction.slug
