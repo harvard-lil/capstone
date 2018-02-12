138b53e2929bca2beef767ea05eca0b6fb5843b2
anastasia <anastasia.aizman@gmail.com>
2018-02-12 17:17:30 -0500

    update - a more extensive permissions check, allowing us to serve whitelisted casetext to everyone

--- capstone/capapi/permissions.py
+++ capstone/capapi/permissions.py
@@ -18,9 +18,30 @@ class IsAPIUser(permissions.BasePermission):
             # boot if user tries anything but `GET`, `OPTIONS`, or `HEAD`
             return False
 
-        # give away metadata to everyone!
-        if not request.query_params.get('type') == 'download':
+        # check whether user wants to get full case text
+        context = view.get_renderer_context()
+        full_case = context.get('request').query_params.get('full_case', 'false').lower()
+        if full_case != 'true':
+            # serve metadata to everyone!
             return True
-        else:
-            # make sure user has auth token before proceeding if they want to download
-            return request.auth
+
+        # get blacklisted case count
+        cases = view.queryset.filter(**context.get('kwargs'), jurisdiction__whitelisted=False)
+        blacklisted_count = cases.count()
+
+        # If the request contains only whitelisted jurisdictions
+        # serve up whether the user is signed in or not
+        if not blacklisted_count:
+            return True
+
+        # from here forth one cannot be anonymous
+        if request.user.is_anonymous():
+            return False
+
+        is_allowed = request.user.case_allowance_remaining >= blacklisted_count
+
+        if is_allowed:
+            # if allowed, discount the number of blacklisted cases served to the user
+            request.user.update_case_allowance(case_count=blacklisted_count)
+        return is_allowed
+
