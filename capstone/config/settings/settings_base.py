d206f5eaa9c27915cf4ffa90520f0da2b94055dd
Jack Cushman <jcushman@law.harvard.edu>
2018-04-03 17:19:03 -0400

    Add whitenoise for static files serving

--- capstone/config/settings/settings_base.py
+++ capstone/config/settings/settings_base.py
@@ -52,6 +52,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
