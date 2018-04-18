c5f1e32d514b11c0301acb65337dbc49a83636fe
Andrew Silva <asilva@law.harvard.edu>
2018-04-17 22:07:23 -0400

    put a test on it

--- capstone/scripts/tests/test_tt_ingest.py
+++ capstone/scripts/tests/test_tt_ingest.py
@@ -1,10 +1,19 @@
+from collections import defaultdict
 import pytest
 from scripts import ingest_tt_data
-
+from capdb.models import Reporter, Jurisdiction
 from django.db import connection
 
 @pytest.mark.django_db
 def test_relink_reporter_jurisdiction(ingest_case_xml):
+
+    def make_reporter_jur_map():
+        output_map = defaultdict(list)
+        for reporter in Reporter.objects.all():
+            output_map[reporter.id].append(Jurisdiction.name)
+        return output_map
+
+    initial_map = make_reporter_jur_map()
     with connection.cursor() as cursor:
         cursor.execute("select count(*) from capdb_reporter_jurisdictions")
         assert cursor.fetchone()[0] == 3
@@ -14,6 +23,10 @@ def test_relink_reporter_jurisdiction(ingest_case_xml):
         ingest_tt_data.relink_reporter_jurisdiction()
         cursor.execute("select count(*) from capdb_reporter_jurisdictions")
         assert cursor.fetchone()[0] == 3
+    final_map = make_reporter_jur_map()
+
+    for reporter_id in initial_map:
+        assert set(initial_map[reporter_id]) == set(final_map[reporter_id])
 
 
 
