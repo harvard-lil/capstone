39269dd9fcc40b9fe595ea77dc1808a2621937ea
anastasia <anastasia.aizman@gmail.com>
2018-04-18 12:52:25 -0400

    remove - unused permissions classes

--- capstone/capapi/permissions.py
+++ capstone/capapi/permissions.py
@@ -1,14 +1,6 @@
 from rest_framework import permissions
 
 
-class AdminUserPermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return view.action == 'retrieve' or request.user.is_admin
-
-
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_admin
 staff_level_permissions = [
     'capdb.change_jurisdiction',
     'capapi.add_capuser',
