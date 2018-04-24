8bc078ab297a777c5320a274855077610a97d26f
Jack Cushman <jcushman@law.harvard.edu>
2018-04-24 11:34:30 -0400

    Upgrade pip-tools

--- capstone/capdb/tests/test_build.py
+++ capstone/capdb/tests/test_build.py
@@ -1,5 +1,5 @@
 # tests to make sure our source repo is consistent, and no build commands need to be run
-
+import os
 import subprocess
 from pathlib import Path
 import pytest
@@ -14,6 +14,8 @@ def test_makemigrations():
     assert out.getvalue() == 'No changes detected\n', "Model changes detected. Please run ./manage.py makemigrations"
 
 def test_pip_compile():
-    result = subprocess.run(["pip-compile", "-n"], stdout=subprocess.PIPE)
+    result = subprocess.run(["pip-compile", "-n"], stdout=subprocess.PIPE,
+                            # strip COV_ environment variables so pip-compile doesn't try to report test coverage
+                            env={k:v for k,v in os.environ.items() if not k.startswith('COV_')})
     existing_requirements = Path('requirements.txt').read_bytes()
     assert result.stdout == existing_requirements, "Changes detected to requirements.in. Please run pip-compile"
