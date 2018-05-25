a46d4febb1dff6385be40734585be22ad9aa3159
Jack Cushman <jcushman@law.harvard.edu>
2018-05-25 15:58:45 -0400

    Split out new columns from code migration for later debugging

--- capstone/capdb/migrations/0040_auto_20180525_1905.py
+++ capstone/capdb/migrations/0040_auto_20180525_1905.py
@@ -45,16 +45,4 @@ class Migration(migrations.Migration):
             name='size',
             field=models.IntegerField(blank=True, null=True),
         ),
-    ] + [
-        # update the `size` column in our xml tables.
-        # drop the `versioning_trigger` trigger during this update -- we don't need copies without size populated.
-        # the versioning trigger will be recreated after migrations by `set_up_postgres`, which is called by `fab migrate`.
-        migrations.RunSQL(
-            """
-                DROP TRIGGER IF EXISTS versioning_trigger ON {table};
-                UPDATE {table} SET size=octet_length(text(orig_xml)) WHERE size IS null;
-            """.format(table="capdb_"+model_name),
-            migrations.RunSQL.noop
-        )
-        for model_name in ['volumexml', 'pagexml', 'casexml', 'volumexml_history', 'pagexml_history', 'casexml_history']
     ]
