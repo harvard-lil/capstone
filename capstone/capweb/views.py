8d76bb21d6d6e349e3e43b44b00a1fc3128208f5
anastasia <anastasia.aizman@gmail.com>
2018-08-17 13:11:30 -0400

    add - using lil site api

--- capstone/capweb/views.py
+++ capstone/capweb/views.py
@@ -1,12 +1,30 @@
+from collections import OrderedDict
 from django.shortcuts import render
+
+from capweb.helpers import get_data_from_lil_site
 
 
 def index(request):
-    return render(request, "index.html", {'page_name': 'index'})
+    news = get_data_from_lil_site(section="news")
+
+    return render(request, "index.html", {
+        'page_name': 'index',
+        'news': news[0:5]
+    })
 
 
 def about(request):
-    return render(request, "about.html", {"page_name": "about"})
+    news = get_data_from_lil_site(section="news")
+    contributors = get_data_from_lil_site(section="contributors")
+    sorted_contributors = {}
+    for contributor in contributors:
+        sorted_contributors[contributor['sort_name']] = contributor
+    sorted_contributors = OrderedDict(sorted(sorted_contributors.items()), key=lambda t: t[0])
+    return render(request, "about.html", {
+        "page_name": "about",
+        "contributors": sorted_contributors,
+        "news": news
+    })
 
 
 def tools(request):
