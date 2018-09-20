f0cb79dfcc5973ef5893d6e02c8c417f1497fad1
Jack Cushman <jcushman@law.harvard.edu>
2018-09-20 09:45:57 -0400

    Subdomains configurable from settings

--- capstone/config/hosts.py
+++ capstone/config/hosts.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from django_hosts import patterns, host
 
-host_patterns = patterns('',
-    host(r'', settings.ROOT_URLCONF, name='default'),
-    host(r'api', 'capapi.api_urls', name='api'),
-)
+hosts = [host(config["subdomain"], config["urlconf"], name=name) for name, config in settings.HOSTS.items()]
+host_patterns = patterns('', *hosts)
