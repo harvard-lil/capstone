3311720586786e5d01ebd566deed15ece6ccd513
anastasia <anastasia.aizman@gmail.com>
2018-10-01 15:16:15 -0400

    fix - subtitle not hidden anymore, fixed highlighting sidebar items too early

--- capstone/static/js/utils.js
+++ capstone/static/js/utils.js
@@ -1,8 +1,9 @@
 /* helper function found in https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling/488073#488073 */
 function isScrolledIntoView(elem) {
+  let offset = 200;
   let docViewTop = $(window).scrollTop();
   let docViewBottom = docViewTop + $(window).height();
   let elemTop = elem.offsetTop;
   let elemBottom = elemTop + $(elem).height();
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  return ((elemBottom <= (docViewBottom - offset)) && (elemTop >= docViewTop));
 }
