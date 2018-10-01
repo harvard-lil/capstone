504ce44a37ba4503567525454153c8212cb200ad
anastasia <anastasia.aizman@gmail.com>
2018-10-01 15:56:21 -0400

    fixed - scroll good

--- capstone/static/js/utils.js
+++ capstone/static/js/utils.js
@@ -1,9 +1,11 @@
 /* helper function found in https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling/488073#488073 */
 function isScrolledIntoView(elem) {
-  let offset = 200;
-  let docViewTop = $(window).scrollTop();
-  let docViewBottom = docViewTop + $(window).height();
-  let elemTop = elem.offsetTop;
-  let elemBottom = elemTop + $(elem).height();
-  return ((elemBottom <= (docViewBottom - offset)) && (elemTop >= docViewTop));
+  let docViewTop = $(window).scrollTop() - $('nav').height();
+
+  /* checking if element is in the top half of the window */
+  let docViewBottom = docViewTop + ($(window).height()/2);
+  let elemBottom = elem.offsetTop + $(elem).height();
+  let elemTop = elemBottom - $(elem).height();
+  console.log(elem.id, "elemTop:", elemTop, "elemBottom:", elemBottom, "docViewTop:", docViewTop, "docViewBottom:", docViewBottom)
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
