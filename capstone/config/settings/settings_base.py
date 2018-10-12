18ed0ac8a7e9baa00c63ad4c3beecacad16ab5e9
Andrew Silva <asilva@law.harvard.edu>
2018-10-12 17:22:58 -0400

    removed fullurl from settings

--- capstone/config/settings/settings_base.py
+++ capstone/config/settings/settings_base.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fullurl',
 
     'django_filters',
     'django_extensions',
