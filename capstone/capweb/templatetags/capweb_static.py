eb8d44dffa91c3748697b2ccdf24f7f05a03b3c5
Andrew Silva <asilva@law.harvard.edu>
2018-10-12 16:09:12 -0400

    added comment to capweb_static function.

--- capstone/capweb/templatetags/capweb_static.py
+++ capstone/capweb/templatetags/capweb_static.py
@@ -6,5 +6,6 @@ register = template.Library()
 
 @register.simple_tag()
 def capweb_static(path):
+    """ This gives the correct, absolute static file URL in all applications."""
     host = reverse("home", args={}, kwargs={}, host="default", scheme='http' if settings.DEBUG else 'https')[:-1]
     return "{}{}{}".format(host, settings.STATIC_URL, path)
