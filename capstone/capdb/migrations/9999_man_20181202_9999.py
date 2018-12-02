ed3b9c39893a332ddc8290731481843316642741
Ari Chivukula <ari@chivuku.la>
2018-12-02 09:28:09 -0800

    Create 9999_man_20181202_9999.py

---
+++ capstone/capdb/migrations/9999_man_20181202_9999.py
@@ -0,0 +1,25 @@
+# TODO (https://github.com/harvard-lil/capstone/pull/709): Finalize fields for actualization
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('capdb', '0057_auto_20181115_1834'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CrossCaseCitation',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('src_case', models.TextField()),
+                ('src_page', models.IntegerField()),
+                ('dst_case', models.TextField()),
+                ('dst_page', models.IntegerField()),
+                ('count', models.IntegerField()),
+            ],
+        ),
+    ]
