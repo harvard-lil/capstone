49f3983dee7cddddfcab72e547a0e6dcb50c7d4f
Ari Chivukula <ari@chivuku.la>
2018-12-17 10:51:33 -0800

    Update 9999_man_20181202_9999.py

--- capstone/capdb/migrations/9999_man_20181202_9999.py
+++ capstone/capdb/migrations/9999_man_20181202_9999.py
@@ -1,3 +1,4 @@
+# TODO (https://github.com/harvard-lil/capstone/pull/709): Properly actualize the migration
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
@@ -10,7 +11,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CitationGraph',
             fields=[
-                # TODO: Expand for long-form citation graph results (count, pages, context, etc.)
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('src_case', models.ForeignKey(null=False, on_delete=models.deletion.CASCADE, related_name='case_metadatas', to='capdb.Case')),
                 ('dst_case', models.ForeignKey(null=False, on_delete=models.deletion.CASCADE, related_name='case_metadatas', to='capdb.Case')),
