bd570610357e497432bcd257834618c7eebfa116
Ari Chivukula <ari@chivuku.la>
2018-12-26 12:35:40 -1000

    Update test_build_citation_graph.py

--- capstone/scripts/tests/test_build_citation_graph.py
+++ capstone/scripts/tests/test_build_citation_graph.py
@@ -5,6 +5,7 @@ from scripts import build_citation_graph
 @pytest.mark.parametrize("casebody, expected_citations", [
     ("", []),
     pytest.param(0, [], marks=pytest.mark.xfail),
+    ("379 U.S. 241", ["379 U.S. 241"])
 ])
 def test_extract_potential_citations_from_casebody(casebody, expected_citations):
     assert build_citation_graph.extract_potential_citations_from_casebody(casebody) == expected_citations
