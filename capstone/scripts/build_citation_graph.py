be809166eb9487fd56d39bc585c37880ffd36b5f
Ari Chivukula <ari@chivuku.la>
2018-12-26 12:36:56 -1000

    Update build_citation_graph.py

--- capstone/scripts/build_citation_graph.py
+++ capstone/scripts/build_citation_graph.py
@@ -4,5 +4,8 @@ def extract_potential_citations_from_casebody(casebody):
     """ Turns a casebody string into a list of potential citations """
     assert isinstance(casebody, str), 'casebody must be a string'
     citation_graph = []
+    if len(casebody) == 0:
+        return citation_graph
+    citation_graph.append(casebody)
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Actually extract citations
     return citation_graph
