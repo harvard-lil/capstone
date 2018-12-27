7cb09ed530421198a1c38bfc51e2ee15d42d12dc
Ari Chivukula <ari@chivuku.la>
2018-12-27 12:02:43 -0600

    Update build_citation_graph.py

--- capstone/scripts/build_citation_graph.py
+++ capstone/scripts/build_citation_graph.py
@@ -9,7 +9,7 @@ from reporters_db import EDITIONS, VARIATIONS_ONLY
 REGEX_LIST = list(EDITIONS.keys()) + list(VARIATIONS_ONLY.keys())
 REGEX_LIST.sort(key=len, reverse=True)
 REGEX_STR = '|'.join(map(re.escape, REGEX_LIST))
-REPORTER_RE = re.compile("([0-9]+\s%s\s[0-9]+)" % REGEX_STR)
+REPORTER_RE = re.compile("\s(%s)\s" % REGEX_STR)
 
 def extract_potential_citations_from_casebody(casebody):
     """ Turns a casebody string into a list of potential citations """
@@ -17,9 +17,12 @@ def extract_potential_citations_from_casebody(casebody):
     citation_graph = []
     if len(casebody) == 0:
         return citation_graph
-    citation_graph.append(casebody)
+    reporters = REPORTER_RE.findall(casebody)
+    for reporter in reporters:
+        found_reporter_re = re.compile("([0-9]+\s%s\s[0-9]+)" % reporter)
+        citation_graph.append(found_reporter_re.findall(casebody))
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Normalize reporter
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Support Id. citation extraction (Id.)
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Support exact page extraction (... at 666)
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Support signal and index extraction (see also ...; cf. ...)
-    return REPORTER_RE.findall(casebody)
+    return citation_graph
