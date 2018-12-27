c078914e8ec739074d87dc87492f05e3d685690e
Ari Chivukula <ari@chivuku.la>
2018-12-27 08:51:24 -0800

    Update build_citation_graph.py

--- capstone/scripts/build_citation_graph.py
+++ capstone/scripts/build_citation_graph.py
@@ -9,7 +9,7 @@ from reporters_db import EDITIONS, VARIATIONS_ONLY
 REGEX_LIST = list(EDITIONS.keys()) + list(VARIATIONS_ONLY.keys())
 REGEX_LIST.sort(key=len, reverse=True)
 REGEX_STR = '|'.join(map(re.escape, REGEX_LIST))
-REPORTER_RE = re.compile("\s(%s)\s" % REGEX_STR)
+REPORTER_RE = re.compile("[0-9]*\s(%s)[0-9]*\s" % REGEX_STR)
 
 def extract_potential_citations_from_casebody(casebody):
     """ Turns a casebody string into a list of potential citations """
@@ -18,5 +18,4 @@ def extract_potential_citations_from_casebody(casebody):
     if len(casebody) == 0:
         return citation_graph
     citation_graph.append(casebody)
-    # TODO(https://github.com/harvard-lil/capstone/pull/709): Actually extract citations
-    return citation_graph
+    return REPORTER_RE.findall(casebody)
