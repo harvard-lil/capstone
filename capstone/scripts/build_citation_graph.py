f11e69461141ae148d5aa61f84b0083dcc68c727
Ari Chivukula <ari@chivuku.la>
2018-12-28 08:41:04 -0800

    Update build_citation_graph.py

--- capstone/scripts/build_citation_graph.py
+++ capstone/scripts/build_citation_graph.py
@@ -16,9 +16,10 @@ SPACING_RE = re.compile("[\s,;:.()[\]{}]+")
 
 @unique
 class __CasebodyToken(Enum):
-    NOOP = 0 # Token used only to break up patterns
-    NUMBER = 1 # [0-9]+
-    REPORTER = 2 # Can be found in `REPORTER_SET`
+    ID = 0 # `Id` style citation
+    NOOP = 1 # Token used only to break up patterns
+    NUMBER = 2 # [0-9]+
+    REPORTER = 3 # Can be found in `REPORTER_SET`
 
 def __tokenize_casebody(casebody):
     """Splits casebody into components and pairs them with `CasebodyToken`s"""
@@ -38,13 +39,14 @@ def __tokenize_casebody(casebody):
                     tokens.append((spacing_split_token, __CasebodyToken.NUMBER))
                 elif spacing_split_token == "":
                     continue
+                elif spacing_split_token.lower() == "id":
+                    tokens.append(("Id", __CasebodyToken.ID))
                 else:
                     tokens.append((spacing_split_token, __CasebodyToken.NOOP))
     return tokens
 
 def __find_citations_in_tokens(tokens):
     """Finds citations within tokens returned by `__tokenize_casebody`"""
-    # TODO(https://github.com/harvard-lil/capstone/pull/709): Support Id. citation extraction (Id.)
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Support exact page extraction (... at 666)
     # TODO(https://github.com/harvard-lil/capstone/pull/709): Support signal and index extraction (see also ...; cf. ...)
     citations = []
@@ -52,6 +54,9 @@ def __find_citations_in_tokens(tokens):
     while idx < len(tokens):
         token_a = tokens[idx]
         idx += 1
+        if token_a[1] == __CasebodyToken.ID:
+            citations.append(token_a[0])
+            continue
         if token_a[1] != __CasebodyToken.NUMBER:
             continue
         if idx == len(tokens):
