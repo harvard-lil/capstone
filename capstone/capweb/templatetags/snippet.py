0037ea065427fd9c8dfe0d29d1694016a9d5a6d2
Andrew Silva <asilva@law.harvard.edu>
2019-01-14 17:45:57 -0500

    removed unnecessary arguments from templatetag

--- capstone/capweb/templatetags/snippet.py
+++ capstone/capweb/templatetags/snippet.py
@@ -4,5 +4,5 @@ from capdb.models import Snippet
 register = template.Library()
 
 @register.simple_tag()
-def snippet(label, *args, **kwargs):
+def snippet(label):
     return Snippet.objects.get(label=label).contents
