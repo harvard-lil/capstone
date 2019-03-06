80378ad0cf79db6995fd47b1d677f88370207d95
Andrew Silva <asilva@law.harvard.edu>
2019-03-05 21:27:19 -0500

    fixed html autoescaping bug

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -1,6 +1,7 @@
 from re import findall
 
 from django import template
+from django.utils.html import escape
 
 from rest_framework.templatetags.rest_framework import urlize_quoted_links
 
@@ -8,7 +9,7 @@ register = template.Library()
 
 @register.filter()
 def urlize_url_fields_only(text):
-    ret = text.decode()
+    ret = escape(text.decode())
     for url in findall('"url": "(http[^"]+)"', text.decode()):
         ret = ret.replace(url, urlize_quoted_links(url))
     return ret
