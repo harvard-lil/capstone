08f6226eac0c5c4724cd6afcf404950007efc816
Andrew Silva <asilva@law.harvard.edu>
2019-03-07 17:02:47 -0500

    cleaned up my imports

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -1,8 +1,7 @@
-from re import findall
+from re import sub
 
 from django import template
 from django.utils.html import escape
-import re
 
 
 from rest_framework.templatetags.rest_framework import urlize_quoted_links
@@ -11,7 +10,7 @@ register = template.Library()
 
 @register.filter()
 def urlize_url_fields_only(text):
-    ret = re.sub(
+    ret = sub(
         '&quot;(http:.*)?(&quot;)',
         lambda url_match: urlize_quoted_links(url_match.group(1)),
         escape(text.decode()))
