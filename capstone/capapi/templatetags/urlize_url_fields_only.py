3d7d3d955648a9db22e55c056d3904aef9a0bb7e
Andrew Silva <asilva@law.harvard.edu>
2019-03-07 16:43:13 -0500

    expanded whitelist

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -2,6 +2,8 @@ from re import findall
 
 from django import template
 from django.utils.html import escape
+import re
+
 
 from rest_framework.templatetags.rest_framework import urlize_quoted_links
 
@@ -9,7 +11,8 @@ register = template.Library()
 
 @register.filter()
 def urlize_url_fields_only(text):
-    ret = escape(text.decode())
-    for url in findall('"url": "(http[^"]+)"', text.decode()):
-        ret = ret.replace(url, urlize_quoted_links(url))
+    ret = re.sub(
+        '&quot;(http:.*)?(&quot;)',
+        lambda url_match: urlize_quoted_links(url_match.group(1)),
+        escape(text.decode()))
     return ret
