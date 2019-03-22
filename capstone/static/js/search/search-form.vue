b5e8d50ededd6c8f9a7e8656c492010e8d519bd2
anastasia <anastasia.aizman@gmail.com>
2019-03-22 10:54:28 -0400

    bump - more styling

--- capstone/static/js/search/search-form.vue
+++ capstone/static/js/search/search-form.vue
@@ -1,128 +1,138 @@
 <template>
   <form @submit.prevent="$emit('new-search', fields, endpoint)" class="row">
-    <div class="search-form-container">
-      <searchroutes :endpoint="endpoint" :endpoints="endpoints"></searchroutes>
-      <br/>
-      <!-- Table showing search fields. Also includes add field and search buttons. -->
+    <div class="col-2"></div>
+    <div class="col-10 search-form-contents">
       <div class="row">
-        <div v-if="search_error"
-             role="alert"
-             class="alert alert-danger">
-          <p>{{search_error}}</p>
-          <h2 id="form-errors-heading" tabindex="-1" class="sr-only">{{search_error}}</h2>
-        </div>
-        <div v-if="Object.keys(field_errors).length"
-             role="alert"
-             class="alert alert-danger">
-          <!--<p>Please correct the following <strong>2 error(s)</strong>: </p>-->
-          <p>Please correct the following {{ Object.keys(field_errors).length }} error(s):</p>
-          <h2 id="form-errors-heading" tabindex="-1" class="sr-only">Please correct the following {{
-            Object.keys(field_errors).length }} error(s)</h2>
-          <ul class="bullets">
-            <li v-for="(error, name) in field_errors"
-                :key="'error' + name">
-              <a :href="'#'+name">{{getFieldByName(name).label}}:</a> {{error}}
-            </li>
-          </ul>
-        </div>
-        <div class="col-12">
-          <div class="row field_row_container"
-               v-for="field in fields" :key="field.name"
-               v-bind:class="{ 'alert-danger': field_errors.hasOwnProperty(field['name']) }">
-            <div class="col-4 field_label_container">
-              <label class="querylabel" :for="field['name']">
-                {{ field["label"] }}
-              </label>
-            </div>
-            <div class="col-6">
-              <template v-if="field['choices']">
-                <select v-model='field["value"]'
-                        :id='field["name"]'>
-                  <option v-for="(label, value) in choices[field['choices']]"
-                          :value="value" v-bind:key="label">
-                    {{label}}
-                  </option>
-                </select>
-              </template>
-              <template v-else>
-                <input v-model='field["value"]'
-                       :class="['queryfield', field_errors[field.name] ? 'is-invalid' : '']"
-                       type="text"
-                       :id='field["name"]'
-                       :placeholder='field["format"] || false'
-                >
-                <div vif="field_errors[field.name]" class="invalid-feedback">
-                  {{ field_errors[field.name] }}
-                </div>
-              </template>
-            </div>
-            <div class="col-1">
-              <div class="remfield">
-                <button type="button"
-                        :class="[fields.length <= 1 ? 'disabled' : 'active', 'field-button']"
-                        :disabled="fields.length <= 1"
-                        @click="removeField(field.name)">
-                </button>
-              </div>
-
-            </div>
-          </div>
-          <!--Add field row-->
+        <div class="search-form-container">
+          <searchroutes :endpoint="endpoint" :endpoints="endpoints">
+          </searchroutes>
+          <br/>
+          <!-- Table showing search fields. Also includes add field and search buttons. -->
           <div class="row">
-            <div class="col-3"></div>
-            <div class="col-7">
-              <div v-if="fields.length > 0" class="dropdown addfield">
-                <button class="dropdown-toggle add-field-button btn-white-violet"
-                        type="button"
-                        id="dropdownMenuButton"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false">
-                  Add Field
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <button class="dropdown-item" type="button"
-                          v-for="newfield in availableFields()" :key="newfield.name"
-                          @click="addField(newfield)">
-                    {{ newfield.label }}
+            <div v-if="search_error"
+                 role="alert"
+                 class="alert alert-danger">
+              <p>{{search_error}}</p>
+              <h2 id="form-errors-heading" tabindex="-1" class="sr-only">{{search_error}}</h2>
+            </div>
+            <div v-if="Object.keys(field_errors).length"
+                 role="alert"
+                 class="alert alert-danger">
+              <!--<p>Please correct the following <strong>2 error(s)</strong>: </p>-->
+              <p>Please correct the following {{ Object.keys(field_errors).length }} error(s):</p>
+              <h2 id="form-errors-heading" tabindex="-1" class="sr-only">
+                Please correct the following {{Object.keys(field_errors).length}} error(s)</h2>
+              <ul class="bullets">
+                <li v-for="(error, name) in field_errors"
+                    :key="'error' + name">
+                  <a :href="'#'+name">{{getFieldByName(name).label}}:</a> {{error}}
+                </li>
+              </ul>
+            </div>
+            <div class="col-12">
+              <div class="row field_row_container"
+                   v-for="field in fields" :key="field.name"
+                   v-bind:class="{ 'alert-danger': field_errors.hasOwnProperty(field['name']) }">
+                <div class="col-3 field_label_container">
+                  <label class="querylabel" :for="field['name']">
+                    {{ field["label"] }}
+                  </label>
+                </div>
+                <div class="col-7">
+                  <template v-if="field['choices']">
+                    <select v-model='field["value"]'
+                            :id='field["name"]'>
+                      <option v-for="(label, value) in choices[field['choices']]"
+                              :value="value" v-bind:key="label">
+                        {{label}}
+                      </option>
+                    </select>
+                  </template>
+                  <template v-else>
+                    <input v-model='field["value"]'
+                           :class="['queryfield', field_errors[field.name] ? 'is-invalid' : '']"
+                           type="text"
+                           :id='field["name"]'
+                           :placeholder='field["format"] || false'
+                    >
+                    <div vif="field_errors[field.name]" class="invalid-feedback">
+                      {{ field_errors[field.name] }}
+                    </div>
+                  </template>
+                </div>
+                <div class="col-1">
+                  <div class="remfield">
+                    <button type="button"
+                            :class="[fields.length <= 1 ? 'disabled' : 'active', 'field-button']"
+                            :disabled="fields.length <= 1"
+                            @click="removeField(field.name)">
+                    </button>
+                  </div>
+
+                </div>
+              </div>
+              <!--Add field row-->
+              <div class="row">
+                <div class="col-3"></div>
+                <div class="col-7">
+                  <div v-if="fields.length > 0" class="dropdown addfield">
+                    <button class="dropdown-toggle add-field-button btn-white-violet"
+                            type="button"
+                            id="dropdownMenuButton"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false">
+                      Add Field
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <button class="dropdown-item" type="button"
+                              v-for="newfield in availableFields()" :key="newfield.name"
+                              @click="addField(newfield)">
+                        {{ newfield.label }}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-1"></div>
+              </div>
+              <!--Submit row-->
+              <div class="row">
+                <div class="col-4"></div>
+                <div class="col-7">
+                  <button class="btn-default btn-submit"
+                          type="submit"
+                          value="Search">
+                    Search
+                    <span v-if="show_loading"
+                          class="spinner-border spinner-border-sm"
+                          role="status"
+                          aria-hidden="true"></span>
                   </button>
+                  <span v-if="show_loading"
+                        id="searching-focus"
+                        class="sr-only"
+                        tabindex="-1">Searching&nbsp;</span>
                 </div>
+                <div class="col-1"></div>
               </div>
-              <button class="btn-default btn-submit"
-                      type="submit"
-                      value="Search">
-                Search
-                <span v-if="show_loading" class="spinner-border spinner-border-sm" role="status"
-                      aria-hidden="true"></span>
-              </button>
-              <span v-if="show_loading" id="searching-focus" class="sr-only" tabindex="-1">Searching&nbsp;</span>
             </div>
-            <div class="col-1"></div>
           </div>
-          <!--Submit row-->
-          <div class="row">
-            <div class="col-4"></div>
-            <div class="col-7">
-
-            </div>
-            <div class="col-1"></div>
-          </div>
+        </div>
+        <div class="search-disclaimer">
+          <p>
+            Searching U.S. caselaw published through mid-2018. <a :href="urls.search_docs">Documentation</a>.<br>
+          </p>
+          <p>
+            <span class="bold">Need legal advice?</span> This is not your best option! Read about
+            <a :href="urls.search_docs + '#research'">our limitations and alternatives</a>.
+          <hr/>
+          <p>
+            * Note on full text search:
+            Terms will be stemmed and combined using AND. Words in quotes will be searched as a phrase.
+          </p>
+          <br/>
         </div>
       </div>
-    </div>
-    <div class="search-disclaimer">
-      <!--Docs links-->
-      <p>
-        Searching U.S. caselaw published through mid-2018. <a :href="urls.search_docs">Documentation</a>.<br>
-      </p>
-      <p>
-        <strong>Need legal advice?</strong> This is not your best option! Read about
-        <a :href="urls.search_docs + '#research'">our limitations and alternatives</a>.
-      <hr/>
-      <p>
-      * Note on full text search:
-
-      </p>
     </div>
   </form>
 </template>
