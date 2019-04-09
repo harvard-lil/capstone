e97adf69a3131923eb3a788d4a701e7601129f60
Jack Cushman <jcushman@law.harvard.edu>
2019-04-09 12:19:25 -0400

    Refacter set-cookie redirect to preserve url fragment

--- capstone/cite/urls.py
+++ capstone/cite/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 if settings.CITE_SUBDOMAIN_FEATURE:
     urlpatterns = [
+        path('set-cookie/', views.set_cookie, name='set_cookie'),
         path('<str:series_slug>/<str:volume_number>/<str:page_number>/<int:case_id>', views.citation, name='citation'),
         path('<str:series_slug>/<str:volume_number>/<str:page_number>/', views.citation, name='citation'),
         path('<str:series_slug>/<str:volume_number>/', views.volume, name='volume'),
