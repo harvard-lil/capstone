178fc4248c4ffe68cdf0fd2d3df2c33431b13926
Jack Cushman <jcushman@law.harvard.edu>
2019-05-09 17:41:12 -0400

    Cache frontend_url value on CaseMetadata

--- capstone/cite/urls.py
+++ capstone/cite/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('set-cookie/', views.set_cookie, name='set_cookie'),
-    path('<str:series_slug>/<str:volume_number>/<str:page_number>/<int:case_id>', views.citation, name='citation'),
+    path('<str:series_slug>/<str:volume_number>/<str:page_number>/<int:case_id>/', views.citation, name='citation'),
     path('<str:series_slug>/<str:volume_number>/<str:page_number>/', views.citation, name='citation'),
     path('<str:series_slug>/<str:volume_number>/', views.volume, name='volume'),
     path('<str:series_slug>/', views.series, name='series'),
