256a3a319316c9906f7764a6358a4b0998186a82
anastasia <anastasia.aizman@gmail.com>
2019-05-22 09:50:38 -0400

    add - mock dns lookup test

--- capstone/capweb/tests/test_helpers.py
+++ capstone/capweb/tests/test_helpers.py
@@ -1,6 +1,10 @@
 import pytest
+import socket
 from django.test import RequestFactory
 from capweb.helpers import is_google_bot
+
+user_agent_google = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+user_agent_other = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:67.0) Gecko/20100101 Firefox/67.0"
 
 
 @pytest.mark.django_db
@@ -23,17 +27,49 @@ def x_test_is_google_bot_with_dns_lookup(anonymous_user):
     request.user = anonymous_user
 
     # User agent and IP look like Google
-    request.META["HTTP_USER_AGENT"] = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    request.META["HTTP_USER_AGENT"] = user_agent_google
     request.user.ip_address = "66.249.66.1"
     assert is_google_bot(request)
 
     # User agent is not a bot
-    request.META["HTTP_USER_AGENT"] = \
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:67.0) Gecko/20100101 Firefox/67.0"
+    request.META["HTTP_USER_AGENT"] = user_agent_other
+
     assert not is_google_bot(request)
 
     # User agent looks like a google bot but IP address is not a bot
-    request.META["HTTP_USER_AGENT"] = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    request.META["HTTP_USER_AGENT"] = user_agent_google
     request.user.ip_address = "123.456.789"
     assert not is_google_bot(request)
 
+
+@pytest.mark.django_db
+def test_is_google_bot_without_dns_lookup(monkeypatch, anonymous_user):
+    """
+    Stubbing out DNS lookup, returning a google bot host string.
+
+    """
+
+    def mock_get_googlehost(ip):
+        return ["crawl-66-249-66-1.googlebot.com"]
+
+    monkeypatch.setattr(socket, "gethostbyaddr", mock_get_googlehost)
+    request = RequestFactory().get('/')
+    request.user = anonymous_user
+
+    # User agent and IP look like Google
+    request.META["HTTP_USER_AGENT"] = user_agent_google
+    request.user.ip_address = "66.249.66.1"
+    assert is_google_bot(request)
+
+    # User agent is not a bot
+    request.META["HTTP_USER_AGENT"] = user_agent_other
+    assert not is_google_bot(request)
+
+    def mock_get_otherhost(ip):
+        return ["not-googlebot.com"]
+
+    # User agent looks like a google bot but IP address is not a bot
+    monkeypatch.setattr(socket, "gethostbyaddr", mock_get_otherhost)
+    request.META["HTTP_USER_AGENT"] = user_agent_google
+    request.user.ip_address = "123.456.789"
+    assert not is_google_bot(request)
