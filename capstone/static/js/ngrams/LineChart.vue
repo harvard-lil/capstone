0b6c05cf546e3384d39eace642c42531deb97929
Jack Cushman <jcushman@law.harvard.edu>
2019-06-02 21:34:02 -0400

    Ngram viewer updates

--- capstone/static/js/ngrams/LineChart.vue
+++ capstone/static/js/ngrams/LineChart.vue
@@ -5,7 +5,7 @@
 
   export default {
     extends: Line,
-    props: ['chartData'],
+    props: ['chartData', 'percentOrAbs', 'countType', 'smoothingWindow'],
     mixins: [reactiveProp],
     data() {
       return {
@@ -15,6 +15,7 @@
           legend: {
             labels: {
               boxWidth: 20,
+              usePointStyle: true,
             }
           },
           scales: {
@@ -35,16 +36,39 @@
               gridLines: {
                 color: "rgba(0, 0, 0, 0)",
               },
-              ticks: {
-                beginAtZero: true,
-              }
             }]
-          }
+          },
+          tooltips: {
+            callbacks: {
+              title: (tooltipItem, data) => {
+                /* format tooltip title to include date range when smoothing is on */
+                const label = tooltipItem[0].label;
+                if (!this.smoothingWindow)
+                  return label;
+                const startRange = Math.max(data.labels[0], Number(label)-this.smoothingWindow);
+                const endRange = Math.min(data.labels[data.labels.length-1], Number(label)+this.smoothingWindow);
+                return `${startRange}-${endRange}`;
+              },
+              label: (tooltipItem, data) => {
+                /*
+                  format tooltip text based on percentOrAbs and countType,
+                  like "term: X% of instances" or "term: Y cases"
+                */
+                let label = data.datasets[tooltipItem.datasetIndex].label || '';
+                if (label)
+                  label += ': ';
+                if (this.percentOrAbs === "percent") {
+                  label += tooltipItem.yLabel.toPrecision(3) + "% of";
+                }else {
+                  label += tooltipItem.yLabel;
+                }
+                label += this.countType === "count" ? " instances" : " cases";
+                return label;
+              }
+            }
+        }
         }
       }
-    },
-    mounted() {
-      this.renderChart(this.chartData, this.options)
     }
   }
 </script>
