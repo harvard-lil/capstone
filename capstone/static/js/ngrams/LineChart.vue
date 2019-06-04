87f2498cb20d2d90aa09b68dec7110ffb2c4d1b9
Jack Cushman <jcushman@law.harvard.edu>
2019-06-04 11:29:40 -0400

    Add ability to scale line chart vertically

--- capstone/static/js/ngrams/LineChart.vue
+++ capstone/static/js/ngrams/LineChart.vue
@@ -1,73 +1,17 @@
 <script>
   import {Line, mixins} from 'vue-chartjs'
 
-  const {reactiveProp} = mixins;
-
   export default {
     extends: Line,
-    props: ['chartData', 'percentOrAbs', 'countType', 'smoothingWindow'],
-    mixins: [reactiveProp],
-    data() {
-      return {
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            labels: {
-              boxWidth: 20,
-              usePointStyle: true,
-            }
-          },
-          scales: {
-            yAxes: [{
-              gridLines: {
-                color: "rgba(0, 0, 0, 0)",
-              },
-              ticks: {
-                beginAtZero: true,
-                callback: function (value) {
-                  if (value % 1 === 0) {
-                    return value;
-                  }
-                }
-              }
-            }],
-            xAxes: [{
-              gridLines: {
-                color: "rgba(0, 0, 0, 0)",
-              },
-            }]
-          },
-          tooltips: {
-            callbacks: {
-              title: (tooltipItem, data) => {
-                /* format tooltip title to include date range when smoothing is on */
-                const label = tooltipItem[0].label;
-                if (!this.smoothingWindow)
-                  return label;
-                const startRange = Math.max(data.labels[0], Number(label)-this.smoothingWindow);
-                const endRange = Math.min(data.labels[data.labels.length-1], Number(label)+this.smoothingWindow);
-                return `${startRange}-${endRange}`;
-              },
-              label: (tooltipItem, data) => {
-                /*
-                  format tooltip text based on percentOrAbs and countType,
-                  like "term: X% of instances" or "term: Y cases"
-                */
-                let label = data.datasets[tooltipItem.datasetIndex].label || '';
-                if (label)
-                  label += ': ';
-                if (this.percentOrAbs === "percent") {
-                  label += tooltipItem.yLabel.toPrecision(3) + "% of";
-                }else {
-                  label += tooltipItem.yLabel;
-                }
-                label += this.countType === "count" ? " instances" : " cases";
-                return label;
-              }
-            }
-        }
-        }
+    mixins: [mixins.reactiveProp],
+    props: ["options"],
+    methods: {
+      fullRerender(chartData, options) {
+        // this exists only as a workaround for non-reactive properties that need to be updated,
+        // currently just yAxisID -- https://github.com/apertureless/vue-chartjs/issues/177
+        if (this.$data._chart)
+          this.$data._chart.destroy();
+        this.renderChart(chartData, options)
       }
     }
   }
