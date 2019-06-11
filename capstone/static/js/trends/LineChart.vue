6b935f76515f1a0f87d1a5c3271947e4b998ea6a
Jack Cushman <jcushman@law.harvard.edu>
2019-06-10 22:05:00 -0400

    Ngram viewer updates

--- capstone/static/js/trends/LineChart.vue
+++ capstone/static/js/trends/LineChart.vue
@@ -5,14 +5,5 @@
     extends: Line,
     mixins: [mixins.reactiveProp],
     props: ["options"],
-    methods: {
-      fullRerender(chartData, options) {
-        // this exists only as a workaround for non-reactive properties that need to be updated,
-        // currently just yAxisID -- https://github.com/apertureless/vue-chartjs/issues/177
-        if (this.$data._chart)
-          this.$data._chart.destroy();
-        this.renderChart(chartData, options)
-      }
-    }
   }
 </script>
