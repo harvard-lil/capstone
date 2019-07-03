e8600ed6ecaafc32f220e17b4afd4057729fd60c
Jack Cushman <jcushman@law.harvard.edu>
2019-07-03 14:51:30 -0400

    Avoid URL double-escape in API browser

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -1,17 +1,19 @@
-from re import sub
+import re
 
 from django import template
 from django.utils.html import escape
-
+from django.utils.safestring import mark_safe
 
 from rest_framework.templatetags.rest_framework import urlize_quoted_links
+
 
 register = template.Library()
 
 @register.filter()
 def urlize_url_fields_only(text):
-    ret = sub(
-        '&quot;(https?://.*)&quot;',
-        lambda url_match: '&quot;%s&quot;' % urlize_quoted_links(url_match.group(1)),
-        escape(text.decode()))
-    return ret
+    """
+        Split text into quoted urls, and other. Other text is escaped normally; quoted urls are passed through
+        urlize_quoted_links(), which performs escaping.
+    """
+    pieces = re.split(r'("https?://\S+")', text.decode())
+    return mark_safe(''.join(urlize_quoted_links(piece) if i % 2 else escape(piece) for i, piece in enumerate(pieces)))
