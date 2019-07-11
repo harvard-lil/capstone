2b23038f4766d8d3366d471c75ae9555494fb8f1
Andrew Silva <asilva@law.harvard.edu>
2019-07-11 14:11:45 -0400

    added image path

--- capstone/capweb/models.py
+++ capstone/capweb/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Gallery(models.Model):
     title = models.CharField(max_length=255) # markdown-formatted text
+    content = models.TextField() # markdown-formatted text
+    image_path = models.CharField(max_length=255)
     page_link = models.CharField(max_length=255, blank=True, null=True)
     repo_link = models.CharField(max_length=255, blank=True, null=True)
-    content = models.TextField() # markdown-formatted text
 
     def __str__(self):
         return self.label
