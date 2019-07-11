ebf09a0e18868498cff7cc7cd0ca1eea6e2b04cb
Andrew Silva <asilva@law.harvard.edu>
2019-07-11 13:50:01 -0400

    added gallery model

--- capstone/capweb/models.py
+++ capstone/capweb/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+from django.db import models
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=255) # markdown-formatted text
+    page_link = models.CharField(max_length=255, blank=True, null=True)
+    repo_link = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField() # markdown-formatted text
+
+    def __str__(self):
+        return self.label
