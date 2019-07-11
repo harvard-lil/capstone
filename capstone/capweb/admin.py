45b88467424c802e069831d9f3c0d2db76d18ad7
Andrew Silva <asilva@law.harvard.edu>
2019-07-11 18:41:56 -0400

    added django admin entry

--- capstone/capweb/admin.py
+++ capstone/capweb/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+from capweb.models import Gallery
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'image_path', 'page_link', 'repo_link']
+admin.site.register(Gallery, GalleryAdmin)
