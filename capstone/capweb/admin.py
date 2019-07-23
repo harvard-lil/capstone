61eb53c6c139f6550ab13d91eb5fc4f4d31cef0a
Andrew Silva <asilva@law.harvard.edu>
2019-07-23 15:13:06 -0400

    updated admin

--- capstone/capweb/admin.py
+++ capstone/capweb/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from capweb.models import Gallery
+from capweb.models import GallerySection, GalleryEntry
 
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ['title', 'section', 'content', 'image', 'page_link', 'repo_link']
-admin.site.register(Gallery, GalleryAdmin)
+
+admin.site.register(GalleryEntry, GalleryAdmin)
+
+
+class GallerySectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order']
+
+
+admin.site.register(GallerySection, GallerySectionAdmin)
