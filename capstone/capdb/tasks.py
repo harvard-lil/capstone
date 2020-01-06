0ac65dfe3708da696cfac3694efd92f377c7d6b1
Andrew Silva <asilva@law.harvard.edu>
2020-01-06 17:01:55 -0500

    updated to use update_elasticsearch_for_vol instead of model method

--- capstone/capdb/tasks.py
+++ capstone/capdb/tasks.py
@@ -133,7 +133,7 @@ def update_volume_number_slugs(barcode):
                 original_slug, vol.volume_number, vol.volume_number_slug, vol.case_metadatas.count(), barcode))
         else:
             print("Updated {} Model and ES with new slug value.".format(barcode))
-            CaseMetadata.reindex_cases([ case for case in vol.case_metadatas.all() ])
+            update_elasticsearch_for_vol(vol.id)
 
 
 
