4420376b72425c394c638f03bd4dcf5196354128
Ben Steinberg <bsteinberg@law.harvard.edu>
2020-02-06 16:09:33 -0500

    Fix typo

--- capstone/capweb/views.py
+++ capstone/capweb/views.py
@@ -304,7 +304,7 @@ def download_files(request, filepath=""):
             context = {
                 "filename": filepath,
                 "error": "If you believe you should have access to this file, "
-                         "please let is know at <a href='mailto:info@case.law'>info@case.law</a>.",
+                         "please let us know at <a href='mailto:info@case.law'>info@case.law</a>.",
                 "title": "403 - Access to this file is restricted",
             }
             return render(request, "file_download_400.html", context, status=403)
