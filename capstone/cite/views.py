2b1a2f84b0058a8c00595c6a841c32aa8a3aea60
Jack Cushman <jcushman@law.harvard.edu>
2020-02-26 14:38:29 -0500

    Feature flag to disable case pdfs by default

--- capstone/cite/views.py
+++ capstone/cite/views.py
@@ -238,7 +238,7 @@ def citation(request, series_slug, volume_number_slug, page_number, case_id=None
     # handle pdf output --
     # wait until here to do this so serializer() can apply case quotas
     db_case = db_case or CaseMetadata.objects.select_related('volume').prefetch_related('citations').get(pk=case.id)
-    can_render_pdf = db_case.volume.pdf_file and not db_case.no_index_redacted
+    can_render_pdf = db_case.volume.pdf_file and not db_case.no_index_redacted and settings.CASE_PDF_FEATURE
     if pdf:
         if serialized_data['casebody']['status'] != 'ok':
             return HttpResponseRedirect(db_case.get_full_frontend_url())
