c8f7e4a99fb26093ac807c3b2482d9d26e43e580
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-02-26 21:23:32 -0500

    add - transaction.atomic??

--- capstone/scripts/extract_citations.py
+++ capstone/scripts/extract_citations.py
@@ -1,7 +1,9 @@
 import re
 from capapi.documents import CaseDocument
+from django.db import transaction
 from reporters_db import REPORTERS, VARIATIONS_ONLY
-from capdb.models import ExtractedCitation, Reporter, VolumeMetadata, Citation, Jurisdiction, CaseMetadata, CaseBodyCache
+from capdb.models import ExtractedCitation, Reporter, VolumeMetadata, Citation, Jurisdiction, CaseMetadata, \
+    CaseBodyCache
 
 
 def extract(case_text):
@@ -26,7 +28,6 @@ def extract(case_text):
                                        "page_num": page_num}
         else:
             citation_misses.append(citation)
-    print("extract, hits:", len(citation_hits.keys()), len(citation_misses))
     return citation_hits, citation_misses
 
 
@@ -42,47 +43,43 @@ def extract_citations_from_casedoc(casedoc, volume_id, update_existing=False):
     # TODO: decide what to do with the misses
     for citation, cite_parts in hits.items():
         reporter_str = cite_parts["reporter"]
-        cite, created = ExtractedCitation.objects.get_or_create(original_cite=citation)
-        cite.case_origins.add(casedoc.id)
-        if created or update_existing:
-            reporters_to_check = []
-            for rep_instance in REPORTERS[reporter_str]:
-                reporters_to_check += list(rep_instance['variations'].keys())
-            reporter = find_reporter_match(reporter_str, reporters_to_check)
-            if reporter:
-                cite.reporter_match = reporter
-                try:
-                    cite.volume_match = VolumeMetadata.objects.get(
-                        reporter=reporter,
-                        volume_number=cite_parts["vol_num"]
-                    )
-                except VolumeMetadata.DoesNotExist:
-                    pass
+        with transaction.atomic():
+            cite, created = ExtractedCitation.objects.get_or_create(original_cite=citation)
+            cite.case_origins.add(casedoc.id)
+            if created or update_existing:
+                reporters_to_check = []
+                for rep_instance in REPORTERS[reporter_str]:
+                    reporters_to_check += list(rep_instance['variations'].keys())
+                reporter = find_reporter_match(reporter_str, reporters_to_check)
+                if reporter:
+                    cite.reporter_match = reporter
+                    try:
+                        cite.volume_match = VolumeMetadata.objects.get(
+                            reporter=reporter,
+                            volume_number=cite_parts["vol_num"]
+                        )
+                    except VolumeMetadata.DoesNotExist:
+                        pass
 
-            cite.reporter_original_string = cite_parts["reporter_original_string"]
-            cite.volume_original_number = cite_parts["vol_num"]
-            cite.save()
-            cite.page_original_number = cite_parts["page_num"]
-            try:
-                cite.citation_match = Citation.objects.get(cite=citation)
-            except Citation.DoesNotExist:
-                pass
-            cite.save()
+                cite.reporter_original_string = cite_parts["reporter_original_string"]
+                cite.volume_original_number = cite_parts["vol_num"]
+                cite.save()
+                cite.page_original_number = cite_parts["page_num"]
+                try:
+                    cite.citation_match = Citation.objects.get(cite=citation)
+                except Citation.DoesNotExist:
+                    pass
+                cite.save()
 
 
 def find_reporter_match(reporter_str, remaining_list_to_check):
-    # print(">>>>>>>>>>", reporter_str, remaining_list_to_check)
     try:
         reporter = Reporter.objects.get(short_name=reporter_str)
         return reporter
     except Reporter.DoesNotExist:
         # TODO: going from, potentially, least likely to most. Need to reverse!
         if len(remaining_list_to_check):
-            find_reporter_match(remaining_list_to_check.pop(), remaining_list_to_check)
+            new_reporter_str = remaining_list_to_check.pop()
+            find_reporter_match(new_reporter_str, remaining_list_to_check)
         else:
             return
-
-
-
-
-
