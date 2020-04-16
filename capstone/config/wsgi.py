68709682891e280dc87deedba2f08ac0eede2ef0
Rebecca Cremona <rcremona@law.harvard.edu>
2020-04-16 15:50:47 -0400

    Retry emails if first attempt to send fails.

--- capstone/config/wsgi.py
+++ capstone/config/wsgi.py
@@ -13,4 +13,17 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# patch email sending to retry on error, to work around sporadic connection issues
+from django.core.mail import EmailMessage
+from smtplib import SMTPException
+from time import sleep
+_orig_send = EmailMessage.send
+def retrying_send(message, *args, **kwargs):
+    try:
+        return _orig_send(message, *args, **kwargs)
+    except (SMTPException, TimeoutError):
+        sleep(1)
+        return _orig_send(message, *args, **kwargs)
+EmailMessage.send = retrying_send
+
 application = get_wsgi_application()
