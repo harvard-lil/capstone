05e010c96f1ade215f62ba1d5d1887b244a91f8d
Jack Cushman <jcushman@law.harvard.edu>
2020-06-11 10:19:24 -0400

    Separate pytest settings file; don't use settings.py when running pytest
    ... to avoid "tests run on my machine but not on CI" type problems

--- capstone/config/settings/settings_dev.py
+++ capstone/config/settings/settings_dev.py
@@ -58,16 +58,7 @@ STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
 MAINTAIN_ELASTICSEARCH_INDEX = False
 
 import sys
-if 'pytest' in sys.modules:
-    ## settings only for tests
-    MAINTAIN_ELASTICSEARCH_INDEX = False  # tests must opt in
-    ELASTICSEARCH_INDEXES['cases_endpoint'] = 'cases_test'
-    STORAGES['download_files_storage']['kwargs']['location'] = os.path.join(BASE_DIR, 'test_data/downloads')
-
-    # don't waste time on whitenoise for tests
-    MIDDLEWARE = [i for i in MIDDLEWARE if not i.startswith('whitenoise.')]
-
-else:
+if 'pytest' not in sys.modules:
     ## settings not for tests
     ELASTICSEARCH_INDEXES['cases_endpoint'] = 'cases'
 
