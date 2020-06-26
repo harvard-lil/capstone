07ba86fa7f4ca0745570143df25c5f1b1af197b9
Jack Cushman <jcushman@law.harvard.edu>
2020-06-26 15:58:06 -0400

    Add description field for gallery sections; admin tweaks

--- capstone/capweb/views.py
+++ capstone/capweb/views.py
@@ -126,7 +126,7 @@ def gallery(request):
     sections = GallerySection.objects.prefetch_related(
         Prefetch('entries', queryset=GalleryEntry.objects.filter(featured=True))).order_by('order')
 
-    return render(request, 'gallery.html', {
+    return render(request, 'gallery/gallery.html', {
         'sections': sections,
         'email': settings.DEFAULT_FROM_EMAIL,
         'page_image': 'img/og_image/gallery.png',
@@ -140,7 +140,7 @@ def gallery_section(request, section_slug):
 
     section = get_object_or_404(GallerySection.objects.prefetch_related('entries'), title_slug=section_slug)
 
-    return render(request, 'gallery_section.html', {
+    return render(request, 'gallery/gallery_section.html', {
         'section': section,
         'page_image': 'img/og_image/gallery.png',
         'meta_description': 'Caselaw Access Project Gallery: ' + section.title
