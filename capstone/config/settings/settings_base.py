85325f156b836b653ed40666fae79d9d36e2e4e3
Jack Cushman <jcushman@law.harvard.edu>
2020-07-31 16:34:35 -0400

    Speed tweaks based on profiling tests

--- capstone/config/settings/settings_base.py
+++ capstone/config/settings/settings_base.py
@@ -554,6 +554,9 @@ CELERY_RESOURCE_LIMITS = [
 # security
 SECURE_CONTENT_TYPE_NOSNIFF = True
 MAKE_HTTPS_URLS = True
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+]
 
 SILENCED_SYSTEM_CHECKS = [
     "models.E004"   # For our history tables, the "id" field should not be a primary key. This disables the Django system
