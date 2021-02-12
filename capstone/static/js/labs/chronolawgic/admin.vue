7bb14855f47afba751938446ff157cbb9eeee51d
Andrew Silva <asilva@law.harvard.edu>
2021-02-11 22:14:02 -0500

    modified urls

---
+++ capstone/static/js/labs/chronolawgic/admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    This is a lovely world in which people can see their timelines, delete their timelines, and create their timelines.
+  </main>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Overview',
+  components: {
+  },
+
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
