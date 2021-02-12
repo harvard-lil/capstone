eadd71e99ebc1e207fbc8081fc46b4fac7b95cd8
Andrew Silva <asilva@law.harvard.edu>
2021-02-11 22:19:36 -0500

    Added vuex store and router— not working

--- capstone/static/js/labs/chronolawgic/admin.vue
+++ capstone/static/js/labs/chronolawgic/admin.vue
@@ -8,7 +8,7 @@
 <script>
 
 export default {
-  name: 'Overview',
+  name: 'Admin',
   components: {
   },
 
--- capstone/static/js/labs/chronolawgic/store.js
+++ capstone/static/js/labs/chronolawgic/store.js
@@ -1,78 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import ClientConfig from './chronolawgic-client-config.json'
 import axios from "axios";
+
+
+// eslint-disable-next-line
+const import_urls = urls; // defined in timeline.html
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
-        placeholder: 'placeholder',
-        authentication: {
-            "name": "test user", // what is their name?
-            "token": false, // What is their server authentication token?
-            "admin_users": false, // Can this user administer users?
-            "can_create": false, // Can this user create new timelines?
-            "timelines": [] // What timelines can this user edit?
+        urls: { // Doing this the long way to make it a little easier to see what's going on.
+            chronolawgic_api_create: import_urls.chronolawgic_api_create,
+            chronolawgic_api_retrieve: import_urls.chronolawgic_api_retrieve,
+            chronolawgic_api_update: import_urls.chronolawgic_api_update,
+            chronolawgic_api_delete: import_urls.chronolawgic_api_delete,
+            static: import_urls.static
         },
         available_timelines: [
             //TODO: store a list of timeline titles/ids available on the server
         ],
-        timeline: {
-            //TODO: timeline structure
-
-            id: 1,
-            title: "Timeline Title",
-            created_by: "Editable Text", // (user accts are for auth/logging purposes)
-            categories: {
-                Case: {id: "1", color: "#FF9911"},
-                Legislation: {id: "2", color: "#99FF11"},
-                ExecutiveOrder: {id: "3", color: "#FF9988"},
-                Anarchism: {id: "4", color: "#11FF99"},
-                Police: {id: "5", color: "#8899FF"},
-                Fascism: {id: "6", color: "#1199FF"},
-            },
-            events: {
-                0: {
-                    title: "Case 1",
-                    categories: [1, 3],
-                    cap_link: "https://cite.case.law/ill/1/176/",
-                    link: false,
-                    description: "Though the Court upheld a conviction for membership in a group that advocated the overthrow of the state, Justice Brandeis explained, in a separate opinion, that under the \"clear and present danger test\" the strong presumption must be in favor of \"more speech, not enforced silence.\" That view, which ultimately prevailed, laid the groundwork for modern First Amendment law.",
-                    start_year: 1880,
-                    thumb: 123,
-                },
-                1: {
-                    title: "Case 2",
-                    categories: [1, 2],
-                    cap_link: "https://cite.case.law/ill/1/176/",
-                    link: false,
-                    description: "Our first Supreme Court landmark. Though upholding the defendant's conviction for distributing his call to overthrow the government, the Court held, for the first time, that the Fourteenth Amendment \"incorporates\" the free speech clause of the First Amendment and is, therefore, applicable to the states.",
-                    start_year: 1881,
-                    thumb: 123,
-                    // What else do we need? Disposition? Collapsed/Visible? Hidden? Draft? Author?
-                },
-                2: {
-                    title: "Case 3",
-                    categories: [1, 2],
-                    cap_link: "https://cite.case.law/ill/1/176/",
-                    link: false,
-                    description: "Our first Supreme Court landmark. Though upholding the defendant's conviction for distributing his call to overthrow the government, the Court held, for the first time, that the Fourteenth Amendment \"incorporates\" the free speech clause of the First Amendment and is, therefore, applicable to the states.",
-                    start_year: 1880,
-                    thumb: 123,
-                    // What else do we need? Disposition? Collapsed/Visible? Hidden? Draft? Author?
-                }
-            },
-            years: {1880: [0, 2], 1881: [1]},
-            app_meta: { // DOESN'T GET EXPORTED — implementation specific
-                acl: [],
-                created_by: 1,
-                is_draft: true,
-                is_hidden: false,
-            },
+        id: 1,
+        title: "Timeline Title",
+        CreatedBy: "Editable Text", // (user accts are for auth/logging purposes)
+        categories: {
+            Case: {id: "1", color: "#FF9911"},
+            Legislation: {id: "2", color: "#99FF11"},
+            ExecutiveOrder: {id: "3", color: "#FF9988"},
+            Anarchism: {id: "4", color: "#11FF99"},
+            Police: {id: "5", color: "#8899FF"},
+            Fascism: {id: "6", color: "#1199FF"},
         },
-        updated_timeline: {
-            //TODO: updated timeline structure to store edits
-        }
+        events: [ {
+                name: "Event 1",
+                url: "https://cite.case.law/ill/1/176/",
+                description: "Between some time and some other time, this thing happened.",
+                start_year: 1880,
+                start_month: 1,
+                start_day: 15,
+                end_year: 1889,
+                categories: [2, 4],
+                end_month: 1,
+                end_day: 15,
+        }],
+        cases: [
+            {
+                name: "Case 1",
+                subhead: "Case 1",
+                description: "Though the Court upheld a conviction for membership in a group that advocated the overthrow of the state, Justice Brandeis explained, in a separate opinion, that under the \"clear and present danger test\" the strong presumption must be in favor of \"more speech, not enforced silence.\" That view, which ultimately prevailed, laid the groundwork for modern First Amendment law.",
+                decision_date: "",
+                categories: [1, 3],
+                url: "https://cite.case.law/ill/1/176/",
+                jurisdiction: "Ill.",
+                reporter:  "Ill.",
+                isCap: true,
+                year: 1885,
+                month: 12,
+                day: 30,
+            },
+        ]
     },
     mutations: {
         writeTimeline(state) {
@@ -105,15 +90,28 @@ const store = new Vuex.Store({
         },
     },
     actions: {
-        authenticate(credentials = false) {
-            console.log("authenticating with :")
-            console.log(credentials)
-            return false
+        deserialize: (json) => {
+            this.state.id = json.id
+            this.state.title = json.title
+            this.state.CreatedBy = json.CreatedBy
+            this.state.categories = json.categories
+            this.state.events = json.events
+            this.state.cases = json.cases
+        },
+        serialize: () => {
+            return {
+                id: this.state.id,
+                title: this.state.title,
+                CreatedBy: this.state.CreatedBy,
+                categories: this.state.categories,
+                events: this.state.events,
+                cases: this.state.cases,
+            }
         },
         getTimeLine: function ({commit}) {
             //TODO make this work with vue router so it gets the new timeine as the route changes
             axios
-                .get(ClientConfig.server_url)
+                .get(this.state.urls.chronolawgic_api_retrieve)
                 .then(response => response.data)
                 .then(timeline => {
                     console.log(timeline);
