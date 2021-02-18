b88a4b0f63290af99c0a4b3b43901b1226e64084
Andrew Silva <asilva@law.harvard.edu>
2021-02-18 12:47:35 -0500

    update vuex, made routes work

---
+++ capstone/static/js/labs/chronolawgic/app.vue
@@ -0,0 +1,34 @@
+<template>
+  <main :class = "($store.getters.requestStatus === 'pending')?'loading':''">
+    <router-view>
+
+    </router-view>
+  </main>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Admin',
+  components: {
+  },
+
+  data() {
+    return {
+    }
+  },
+  created() {
+    this.$store.dispatch('requestTimelineList');
+    if (Number.isInteger(this.$route.params.timeline)) {
+      this.$store.dispatch('requestTimeline', this.$route.params.timeline);
+    }
+  }
+};
+</script>
+
+<style>
+  main.loading {
+    background-color: lightgray;
+  }
+</style>
