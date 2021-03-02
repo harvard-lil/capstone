add2284d7235973fabd42bef62080c6845deb294
anastasia <anastasia.aizman@gmail.com>
2021-03-02 12:41:47 -0500

    add - event modal for authors (wip: allowing editing of all properties)

--- capstone/static/js/labs/chronolawgic/case.vue
+++ capstone/static/js/labs/chronolawgic/case.vue
@@ -1,44 +1,53 @@
 <template>
-    <article class="case" @click="toggleEventModal(case_data)" tabindex="0">
-        <event-modal
-                v-if="showEventDetails"
-                data-toggle="modal"
-                data-target="event-modal"
-                :modal.sync="showEventDetails"
-                :event="event"
-                :shown="showEventDetails">
-        </event-modal>
+  <article class="case" @click="toggleEventModal(case_data)" tabindex="0">
+    <event-author-modal v-if="showEventDetails && $store.state.isAuthor"
+                        data-toggle="modal"
+                        data-target="event-author-modal"
+                        :modal.sync="showEventDetails"
+                        :event="event"
+                        :shown="showEventDetails">
+    </event-author-modal>
+    <event-modal
+        v-else-if="showEventDetails && !($store.state.isAuthor)"
+        data-toggle="modal"
+        data-target="event-modal"
+        :modal.sync="showEventDetails"
+        :event="event"
+        :shown="showEventDetails">
+    </event-modal>
 
-        <header>
-           {{ case_data.name }}
-        </header>
-        <section class="subhead">
-            {{ case_data.subhead }}
-        </section>
-        <section class="desc">
-            {{ case_data.short_description }}
-        </section>
-    </article>
+    <header>
+      {{ case_data.name }}
+    </header>
+    <section class="subhead">
+      {{ case_data.subhead }}
+    </section>
+    <section class="desc">
+      {{ case_data.short_description }}
+    </section>
+  </article>
 </template>
 
 <script>
-    import EventModal from './event-modal.vue';
-    export default {
-        name: "Case",
-        components: { EventModal },
-        props: ['case_data', 'year_value'],
-        data() {
-          return {
-              showEventDetails: false
-          }
-        },
-        methods: {
-            toggleEventModal(item) {
-                this.showEventDetails =  !this.showEventDetails;
-                this.event = item
-            },
-        }
-
+import EventModal from './event-modal.vue';
+import EventAuthorModal from './event-author-modal.vue';
+export default {
+  name: "Case",
+  components: {EventModal, EventAuthorModal},
+  props: ['case_data', 'year_value'],
+  data() {
+    return {
+      showEventDetails: false,
+      event: {}
     }
+  },
+  methods: {
+    toggleEventModal(item) {
+      this.showEventDetails = !this.showEventDetails;
+      this.event = item
+    },
+  }
+
+}
 </script>
 
---
+++ capstone/static/js/labs/chronolawgic/event-author-modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal" id="event-author-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">AUTHORING MODAL: {{ event.name }} | {{ event.start_date }}</h5>
+          <button type="button" class="close" @click="closeModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h6>{{ event.short_description }}</h6>
+          <hr/>
+          {{ event.long_description }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event-author-modal",
+  props: [
+    'showEventDetails',
+    'shown',
+    'modal',
+    'event',
+  ],
+  methods: {
+    closeModal() {
+      this.$emit('toggleEventModal');
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+#event-author-modal {
+  display: block;
+}
+</style>
