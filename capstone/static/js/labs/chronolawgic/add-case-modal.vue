067be8763de4435fa0d08ae1d96e431a345e5e8a
anastasia <anastasia.aizman@gmail.com>
2021-03-08 16:42:12 -0500

    fix - check for case

--- capstone/static/js/labs/chronolawgic/add-case-modal.vue
+++ capstone/static/js/labs/chronolawgic/add-case-modal.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" v-if="this.case.name">{{ this.case.name }}</h5>
+          <h5 class="modal-title" v-if="this.case">{{ this.case.name }}</h5>
           <h5 class="modal-title" v-else>ADD CASELAW</h5>
           <button type="button" @click.stop="closeModal" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
