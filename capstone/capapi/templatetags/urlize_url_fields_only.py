3060f8f942f4bd1b5f723ce9f5b69354ded2c7da
Ben Steinberg <bsteinberg@law.harvard.edu>
2021-03-22 14:05:58 -0400

    Update jinja2 and DRF

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -1,10 +1,8 @@
 import re
 
 from django import template
-from django.utils.html import escape
+from django.utils.html import escape, urlize
 from django.utils.safestring import mark_safe
-
-from rest_framework.templatetags.rest_framework import urlize_quoted_links
 
 
 register = template.Library()
@@ -13,7 +11,7 @@ register = template.Library()
 def urlize_url_fields_only(text):
     """
         Split text into quoted urls, and other. Other text is escaped normally; quoted urls are passed through
-        urlize_quoted_links(), which performs escaping.
+        urlize(), which performs escaping.
     """
-    pieces = re.split(r'("https?://\S+")', text.decode())
-    return mark_safe(''.join(urlize_quoted_links(piece) if i % 2 else escape(piece) for i, piece in enumerate(pieces)))
+    pieces = re.split(r'("https?://\S+")', text)
+    return mark_safe(''.join(urlize(piece) if i % 2 else escape(piece) for i, piece in enumerate(pieces)))
