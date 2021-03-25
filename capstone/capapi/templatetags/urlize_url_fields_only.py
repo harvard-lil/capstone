d22ee16bc57756ae5532e375048e13833a538e3d
Ben Steinberg <bsteinberg@law.harvard.edu>
2021-03-25 14:59:19 -0400

    Use force_str so we don't have to care if text is bytes

--- capstone/capapi/templatetags/urlize_url_fields_only.py
+++ capstone/capapi/templatetags/urlize_url_fields_only.py
@@ -3,7 +3,7 @@ import re
 from django import template
 from django.utils.html import escape, urlize
 from django.utils.safestring import mark_safe
-
+from django.utils.encoding import force_str
 
 register = template.Library()
 
@@ -13,5 +13,5 @@ def urlize_url_fields_only(text):
         Split text into quoted urls, and other. Other text is escaped normally; quoted urls are passed through
         urlize(), which performs escaping.
     """
-    pieces = re.split(r'("https?://\S+")', text)
+    pieces = re.split(r'("https?://\S+")', force_str(text))
     return mark_safe(''.join(urlize(piece) if i % 2 else escape(piece) for i, piece in enumerate(pieces)))
