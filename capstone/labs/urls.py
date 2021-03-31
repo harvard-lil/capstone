5f7a7a65b21ff0e98a62cd4577b618dedc4d3958
Andrew Silva <asilva@law.harvard.edu>
2021-03-31 10:27:32 -0400

    LABS setting change
    The LABS boolean setting is now the LABS_HIDDEN list. In the labs urls.py, add a check to see if the project name is in the LABS_HIDDEN list. If we want to retire something or it needs to be taken out of production because it's problematic for some reason, just add it to the list in the base settings.

--- capstone/labs/urls.py
+++ capstone/labs/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
 # Project URLs- make discreet groups of URLs for each project
 
-if settings.LABS:
+if 'chronolawgic' in settings.LABS_HIDDEN:
     # # # # chronolawgic # # # #
     urlpatterns += [
         path('chronolawgic/', LabMarkdownView.as_view(template_name='lab/chronolawgic/about-chronolawgic.md'),
