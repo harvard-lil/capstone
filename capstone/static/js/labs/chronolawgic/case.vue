cf370bc6d1421d6cc9e409985a25d422835580ac
anastasia <anastasia.aizman@gmail.com>
2021-04-07 21:06:13 -0400

    update - category styles and placement

--- capstone/static/js/labs/chronolawgic/case.vue
+++ capstone/static/js/labs/chronolawgic/case.vue
@@ -8,11 +8,27 @@
           tabindex="0">
     <article class="case">
       <header>
-        <span class="case-name">{{ case_data.name }}</span>
+          <span class="categories"
+                v-if="case_data.categories && case_data.categories.length">
+            <ul class="list-inline">
+              <li class="list-inline-item"
+                  v-for="category in categories"
+                  title="category.name"
+                  v-bind:key="category.id">
+                <shape-component :width="20"
+                                 :title="category.name"
+                                 :color="category.color"
+                                 :shapetype="category.shape"></shape-component>
+              </li>
+            </ul>
+          </span>
+        <span v-else class="case-name">{{ case_data.name }}</span>
         <a class="case-link" v-if="case_data.url"
            :href="case_data.url" target="_blank" @click.stop>
           <link-case/>
         </a>
+        <span v-if="case_data.categories && case_data.categories.length" class="case-name">{{ case_data.name }}</span>
+
       </header>
       <section class="desc">
         {{ case_data.short_description }}
@@ -26,11 +42,12 @@
 import LinkCase from '../../../../static/img/icons/open_in_new-24px.svg';
 import {EventBus} from "./event-bus.js";
 import store from "./store";
+import ShapeComponent from './shape-component';
 
 export default {
   name: "Case",
   props: ['case_data', 'year_value'],
-  components: {LinkCase},
+  components: {LinkCase, ShapeComponent},
   computed: {
     dataTarget: () => {
       if (store.getters.isMobile || !store.state.isAuthor) {
@@ -39,6 +56,21 @@ export default {
         return '#add-case-modal'
       }
     },
+  },
+  watch: {
+    case_data: {
+      handler(newval) {
+        this.case_data.categories = newval.categories;
+        this.hydrateCategories();
+      },
+      deep: true
+    }
+  },
+  data() {
+    return {
+      categories: [],
+      timelineCategories: store.getters.categories
+    }
   },
   methods: {
     openModal(item) {
@@ -52,8 +84,19 @@ export default {
     },
     repopulateTimeline() {
       this.$parent.repopulateTimeline();
+    },
+    hydrateCategories() {
+      this.categories = [];
+      for (let i = 0; i < this.case_data.categories.length; i++) {
+        this.categories.push(this.timelineCategories.find(cat => cat.id === this.case_data.categories[i]))
+      }
     }
   },
+  mounted() {
+    if (this.case_data.categories) {
+      this.hydrateCategories()
+    }
+  }
 }
 </script>
 
