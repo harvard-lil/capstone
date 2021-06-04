738003a46883e501a150a36c3b278390eb3f66c2
Jack Cushman <jcushman@law.harvard.edu>
2021-06-04 11:19:54 -0400

    Re-enable polyfills

--- capstone/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    ['@vue/cli-plugin-babel/preset', {
-      // disable the default polyfills included by https://www.npmjs.com/package/@vue/babel-preset-app?activeTab=readme#polyfills
-      // because they cause nondeterministic builds. See https://github.com/vuejs/vue-cli/issues/2983
-      polyfills: [],
-    }]
-  ]
-}
