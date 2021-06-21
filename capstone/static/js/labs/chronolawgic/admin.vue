7d453e5d551cdd094fd746fc011eeba6419131b6
anastasia <anastasia.aizman@gmail.com>
2021-06-21 15:42:28 -0400

    fix - fake url and unused variable

--- capstone/static/js/labs/chronolawgic/admin.vue
+++ capstone/static/js/labs/chronolawgic/admin.vue
@@ -22,7 +22,7 @@
         <br/>
         <div class="h2o-import-container">
           <span>Or pre-populate a timeline from <a href="https://opencasebook.org">H2O</a></span>
-          <input v-model="h2oURL" class="form-control" placeholder="Copy paste casebook.org URL here">
+          <input v-model="h2oURL" class="form-control" placeholder="Copy paste opencasebook.org URL here">
 
           <label for="useOriginalUrls"> Use original H2O URLs</label>&nbsp;
           <input v-model="useOriginalURLs" type="checkbox" id="useOriginalUrls" name="useOriginalUrls"
@@ -151,7 +151,6 @@ export default {
       h2oURL: '',
       useOriginalURLs: false,
       showLoading: false,
-      missingCases: [],
       currentLocation: '',
     }
   },
