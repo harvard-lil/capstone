52d3d41296e27ceb3bc817fe8ccd1aebcdfe3eee
Andy Gu <andygu168@gmail.com>
2021-07-20 11:29:39 -0400

    refactor search results code

--- capstone/static/js/api.js
+++ capstone/static/js/api.js
@@ -1,12 +1,8 @@
 import {encodeQueryData} from './utils';
 
 
-export function getApiUrl(api_url, endpoint, params) {
-  return `${api_url}${endpoint}/?${encodeQueryData(params)}`;
-}
-
-export function getApiUrlNoEncode(api_url, endpoint, params) {
-  return `${api_url}${endpoint}/?${params}`;
+export function getApiUrl(api_url, endpoint, params, split_arrays = false) {
+  return `${api_url}${endpoint}/?${encodeQueryData(params, split_arrays)}`;
 }
 
 export function jsonQuery(url) {
