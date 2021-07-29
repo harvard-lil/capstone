aae9984ac5ed9a08315d524e99b28411e7541062
Andy Gu <andygu168@gmail.com>
2021-07-29 13:09:37 -0400

    lint

--- capstone/capapi/resources.py
+++ capstone/capapi/resources.py
@@ -215,7 +215,7 @@ def parallel_execute(search, workers=20, desired_docs=20000, remove_keys=None):
     to elasticsearch.
     """
     def get_next_search_dict(search, i):
-        filtered_search = search.filter('range', **{'analysis.random_bucket': {'gte': i * buckets_per_worker, \
+        filtered_search = search.filter('range', **{'analysis.random_bucket': {'gte': i * buckets_per_worker, 
             'lt': (i + 1) * buckets_per_worker}}).to_dict()
 
         search_blob = {
