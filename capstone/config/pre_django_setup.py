f90650cfe9d2fc2233a8a9c5fd3a1ec712ff8c9a
Jack Cushman <jcushman@law.harvard.edu>
2021-08-10 15:50:34 -0400

    fix TracingDebugWrapper for Django 3.2

--- capstone/config/pre_django_setup.py
+++ capstone/config/pre_django_setup.py
@@ -2,3 +2,6 @@
 
 # patch this here because we have to make sure we do it before any file imports the eyecite library
 import scripts.patch_reporters_db  # noqa
+
+# patch django's CursorDebugWrapper before any other file imports it
+import django_sql_trace  # noqa
