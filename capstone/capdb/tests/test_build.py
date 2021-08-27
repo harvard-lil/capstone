41eb45ac6096436cde0a78eafd47db8e3ad6b94d
Jack Cushman <jcushman@law.harvard.edu>
2021-08-27 16:21:34 -0400

    Fix missing migrations test

--- capstone/capdb/tests/test_build.py
+++ capstone/capdb/tests/test_build.py
@@ -2,18 +2,21 @@
 import os
 import subprocess
 from pathlib import Path
-
 import pytest
-from io import StringIO
-
-from django.core import management
 
 
 @pytest.mark.django_db(databases=['default', 'capdb', 'user_data'])
 def test_makemigrations():
-    out = StringIO()
-    management.call_command('makemigrations', dry_run=True, stdout=out)
-    assert out.getvalue() == 'No changes detected\n', "Model changes detected. Please run ./manage.py makemigrations"
+    # this can't be done as
+    #   management.call_command('makemigrations', dry_run=True, stdout=out)
+    # because no migrations are reported given the test db settings,
+    # so shell out to the real makemigrations
+    out = subprocess.run(
+        ['python', 'manage.py', 'makemigrations', '--dry-run'],
+        capture_output=True,
+        check=True
+    ).stdout.decode('utf8')
+    assert out == 'No changes detected\n', "Model changes detected. Please run ./manage.py makemigrations"
 
 def test_pip_compile():
     existing_requirements = Path('requirements.txt').read_bytes()
