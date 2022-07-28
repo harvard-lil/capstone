723b4fc67cae74b01a252a0c84e6cae349a470a0
Sabelo Mhlambi <sabzo@users.noreply.github.com>
2022-07-28 10:35:38 -0500

    add necessary volumes

--- services/playwright/test.py
+++ services/playwright/test.py
@@ -1,9 +1,23 @@
+import re
+from playwright.sync_api import Page, expect
 
-from playwright.sync_api import sync_playwright
+host = "http://web:8000"
 
-with sync_playwright() as p:
-    browser = p.webkit.launch()
-    page = browser.new_page()
-    page.goto("http://playwright:8000/")
-    page.screenshot(path="example.png")
-    browser.close()
+def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(
+    page: Page):
+    page.goto(host)
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+    # create a locator
+    get_started = page.locator("text=Get Started")
+
+    # Expect an attribute "to be strictly equal" to the value.
+    expect(get_started).to_have_attribute("href", "/docs/intro")
+
+    # Click the get started link.
+    get_started.click()
+
+    # Expects the URL to contain intro.
+    expect(page).to_have_url(re.compile(".*intro"))
