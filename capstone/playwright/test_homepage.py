6d594a5390311d4555ef920ae6050e67ec106c39
Sabelo Mhlambi <sabzo@users.noreply.github.com>
2022-08-09 15:05:24 -0500

    update indentation based on Flake8 rules

--- capstone/playwright/test_homepage.py
+++ capstone/playwright/test_homepage.py
@@ -1,13 +1,12 @@
-import re
 from playwright.sync_api import Page, expect
 
 url = "http://case.test:8000"
 
 def test_state_onclick_loads_correct_page(page: Page, ordered_list_state_abbreviations):
-  page.goto(url)
-  for _state in ordered_list_state_abbreviations:
-    expected_url = '//cite.case.test:8000/#' + _state
-    selector = '#' + _state
-    loc = page.locator(selector)
-    expect(loc).to_have_attribute('href', expected_url)
+    page.goto(url)
+    for _state in ordered_list_state_abbreviations:
+        expected_url = '//cite.case.test:8000/#' + _state
+        selector = '#' + _state
+        loc = page.locator(selector)
+        expect(loc).to_have_attribute('href', expected_url)
 
