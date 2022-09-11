58d437c7dde42cdbdbe1c0c1735d67153aa4ca5a
Sabelo Mhlambi <sabzo@users.noreply.github.com>
2022-09-11 17:57:50 -0500

    add homepage tests

--- capstone/playwright/test_homepage.py
+++ capstone/playwright/test_homepage.py
@@ -1,8 +1,14 @@
 from playwright.sync_api import Page, expect
 
-url = "http://127.0.0.1:8000"
+url = "http://case.test:8000"
+# states that fail, usually the ones where you have to scroll down to click the map
+# or states that are too small on the map
+# there are errors like "playwright._impl._api_types.Error: Element is outside of the viewport"
+# idaho is not at the bottom but playwright clicks "montana" (right above idaho)
+# maryland also being too small, playwright clicks "virginia" instead of "montana"
+skip = ['dakota-territory', 'am-samoa', 'fla', 'guam', 'haw', 'idaho', 'md', 'mich', 'nj', 'n-mar-i', 'pr', 'vi']
 
-def test_state_onclick_loads_correct_page(page: Page, ordered_list_state_abbreviations):
+def test_each_state_on_map_contains_valid_url(page: Page, ordered_list_state_abbreviations):
     page.goto(url)
     for _state in ordered_list_state_abbreviations:
         expected_url = '//cite.case.test:8000/#' + _state
@@ -11,9 +17,25 @@ def test_state_onclick_loads_correct_page(page: Page, ordered_list_state_abbrevi
         expect(loc).to_have_attribute('href', expected_url)
 
 
+def test_alabamba_map_link(page, ordered_list_state_abbreviations):
+    page.on('response', lambda r: r)
+    for _state in ordered_list_state_abbreviations:
+      if _state in skip:
+        continue
+      page.goto("http://case.test:8000")
+      #page.evaluate("window.scroll(1,1000);")
+      selector = 'a#' + _state
+      #if selector[1:] == '#mont' or selector[1:] == '#idaho':
+      #  import pdb; pdb.set_trace()
+      page.locator(selector).click(force=True)
+      assert page.url == "http://cite.case.test:8000/" + selector[1:]
+
+
+"""
+doesn't work because pointer event is intercepted by the svg map, so hovering over a state first triggers the svg point event
 def test_jurisdiction_name_matches_state_hovered(page: Page, ordered_list_state_abbreviations):
     page.goto(url)
-    for _state in ordered_list_state_abbreviations[:1]:
+    for _state in ordered_list_state_abbreviations:
         selector = '#' + _state
         # getting 1st element in array since all the g tags besides the 0th index are nested inside 0th index g tag.
         link = page.locator(selector)
@@ -22,4 +44,4 @@ def test_jurisdiction_name_matches_state_hovered(page: Page, ordered_list_state_
         link.hover()
         left_side_menu = page.locator('.jur_name')
         expect(left_side_menu).to_have_text(_state_name) 
-    
+"""
