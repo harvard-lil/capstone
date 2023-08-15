37885e5cb1fbefe32a350a625f1ab91883a17439
R I B Z <kilbergr@users.noreply.github.com>
2023-08-15 15:18:20 -0400

    rm unnecessary f

--- capstone/scripts/convert_s3.py
+++ capstone/scripts/convert_s3.py
@@ -77,7 +77,7 @@ def export_cases_to_s3(redacted: bool, reporter_id: str) -> None:
 
     # TODO: address reporters that share slug
     if reporter_id in reporter_slug_dict:
-        reporter_prefix = f"{reporter_slug_dict[reporter_id]}"
+        reporter_prefix = reporter_slug_dict[reporter_id]
     else:
         reporter_prefix = f"{reporter.short_name_slug}"
 
